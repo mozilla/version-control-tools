ada38b35c6be6381ca071dbfe6298c485e8e11dc
Ted Mielczarek <ted.mielczarek@gmail.com>
2008-07-29 19:15:31 -0400

    add buglink extension

---
+++ hgext/pushlog-legacy/buglink.py
@@ -0,0 +1,15 @@
+# To enable:
+#
+# [extensions]
+# buglink = /path/to/buglink.py
+
+import re
+from mercurial import templatefilters
+
+bugzilla = r'<a href="https://bugzilla.mozilla.org/show_bug.cgi=\2">\1</a>'
+bug_re = re.compile(r'((?:bug|b=|(?=#?\d{4,}))(?:\s*#?)(\d+))', re.I)
+
+def buglink(x):
+    return bug_re.sub(bugzilla, x)
+
+templatefilters.filters["buglink"] = buglink
