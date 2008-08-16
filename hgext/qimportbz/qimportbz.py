d6b1be05cf2f501c5bbe823f565e97fa6ae5a7bc
Rob Arnold <tellrob@gmail.com>
2008-08-15 17:02:53 -0700

    Started using optparse, added verbose, quiet and dry-run options

--- hgext/qimportbz/qimportbz.py
+++ hgext/qimportbz/qimportbz.py
@@ -6,31 +6,28 @@ import base64
 from xml.etree.ElementTree import fromstring as xmlfromstring
 from subprocess import Popen, PIPE
 import pdb
-
-def printUsage():
-  print "Usage: qimportbz bug-number"
-
-if len(sys.argv) < 2:
-  printUsage()
-  sys.exit(-2)
+from optparse import OptionParser, make_option
 
 def isGoodAttachment(a):
   if "0" == a.attrib['ispatch']: return False
   id = a.find('attachid').text
   desc = a.find('desc').text
-  #print "Found patch %s (%s)" % (id, desc)
+  if options.verbose:
+    print "Found patch %s (%s)" % (id, desc)
   isobsolete = int(a.attrib['isobsolete'])
   if isobsolete:
-    #print "...but it was obsolete"
+    if options.verbose:
+      print "...but it was obsolete"
     return False
   return True
 
-def importPatch(p):
+def importPatch(p, patchname):
   data64 = p.find('data').text
   data = base64.b64decode(data64)
-  desc = p.find('desc').text
-  patchname = "bug%s_%s" % (bugnum, desc.replace(' ', '_'))
   args = ["hg", "qimport", "-n", patchname, "-"]
+  if options.verbose:
+    print "Running %s" % ' '.join(args)
+  if options.readonly: return
   hg = Popen(args, stdin=PIPE)
   hg.stdin.write(data)
   hg.stdin.close()
@@ -48,7 +45,7 @@ def getFlagDesc(p):
     if name == 'review':
       settername = setter[:setter.index('@')]
       descs.append('%s: r%s' % (settername, status))
-    else:
+    elif options.verbose:
       print "Unhandled flag %s" % name
   return ', '.join(descs)
 
@@ -61,44 +58,125 @@ def multiplePatchChoose(patches):
     flags = getFlagDesc(p)
     print "%s: %s %s" % (i+1, desc, flags)
   choicestr = raw_input("Which patches do you want to import? ")
-  choices = [cleanChoice(s) for s in choicestr.split()]
-  #choicestr = raw_input("Import as separate patches (y/n)? ")
-  #separatePatches = 'y' in choicestr.lower()
-  for choice in choices:
-    importPatch(patches[choice-1])
+  return [cleanChoice(s)-1 for s in choicestr.split()]
 
-bugzilla_base = os.environ['BUGZILLA'] if 'BUGZILLA' in os.environ else "bugzilla.mozilla.org"
-bugnum = sys.argv[1]
+class BaseCommand(object):
+  options = []
+  defaults = {}
+  usage = ""
 
-url = "https://%s/show_bug.cgi?ctype=xml&id=%s" % (bugzilla_base, bugnum)
-print ("Fetching from %s..." % url),
-stream = urllib2.urlopen(url)
-data = stream.read()
-print "\bdone"
-#print "Got %s bytes of data" % len(data)
-print "Parsing bug...",
-xml = xmlfromstring(data)
-print "\bdone"
-attachments = xml.findall("bug/attachment")
-patches = [a for a in attachments if isGoodAttachment(a)]
+class Help(BaseCommand):
+  usage = "%prog command [options]"
+  def run(self):
+    parser.print_help()
 
-if len(patches) == 1:
-  importPatch(patches[0])
-elif len(patches) > 1:
-  print "Found multiple patches"
-  multiplePatchChoose(patches)
-# patcheswithreview = [patch for patch in patches
-#                            if len(
-#                               [f for f in patch.findall('flag')
-#                                  if f.attrib['name'] == 'review' and f.attrib['status'] == '+'
-#                               ])]
-# if len(patcheswithreview) == 1:
-#   print "Found one patch"
-#   importPatch(patcheswithreview[0])
-#   exit(-1)
-# elif len(patcheswithreview) > 1:
-#   print "Found multiple patches with r+"
-# else:
-#   print "Found no patches with review"
-else:
-  print "Found no unobsolete patches"
+class Import(BaseCommand):
+  options = []
+  defaults = {}
+  usage = "%prog import [options] bugnumber [bugnumber...]"
+  def do_bug(self, bugnum):
+    url = "https://%s/show_bug.cgi?ctype=xml&id=%s" % (options.bugzilla, bugnum)
+    if options.verbose:
+      print ("Fetching from %s..." % url),
+    stream = urllib2.urlopen(url)
+    data = stream.read()
+    if options.verbose:
+      print "\bdone"
+    #print "Got %s bytes of data" % len(data)
+    if options.verbose:
+      print "Parsing bug...",
+    xml = xmlfromstring(data)
+    if options.verbose:
+      print "\bdone"
+    attachments = xml.findall("bug/attachment")
+    patches = [a for a in attachments if isGoodAttachment(a)]
+
+    def getPatchName(p):
+      desc = p.find('desc').text
+      return "bug%s_%s" % (bugnum, desc.replace(' ', '_'))
+
+    if len(patches) == 1:
+      patch = patches[0]
+      importPatch(patch, getPatchName(patch))
+    elif len(patches) > 1:
+      if options.verbose:
+        print "Found multiple patches"
+      choices = multiplePatchChoose(patches)
+      #choicestr = raw_input("Import as separate patches (y/n)? ")
+      #separatePatches = 'y' in choicestr.lower()
+      for choice in choices:
+        patch = patches[choice]
+        importPatch(patch, getPatchName(patch))
+    # patcheswithreview = [patch for patch in patches
+    #                            if len(
+    #                               [f for f in patch.findall('flag')
+    #                                  if f.attrib['name'] == 'review' and f.attrib['status'] == '+'
+    #                               ])]
+    # if len(patcheswithreview) == 1:
+    #   print "Found one patch"
+    #   importPatch(patcheswithreview[0])
+    #   exit(-1)
+    # elif len(patcheswithreview) > 1:
+    #   print "Found multiple patches with r+"
+    # else:
+    #   print "Found no patches with review"
+    else:
+      if options.verbose:
+        print "Found no unobsolete patches"
+  def run(self):
+    for bugnum in positional_args:
+      self.do_bug(bugnum)
+
+
+if __name__ == '__main__':
+
+  if len(sys.argv) < 2:
+    printUsage()
+    sys.exit(-2)
+
+  command = sys.argv[1]
+  args = sys.argv[2:]
+
+  if len(command) > 0 and command[0] == '-':
+    args.insert(0, command)
+    command = ''
+
+  try:
+    int(command)
+    args.insert(0, command)
+    command = "import"
+  except ValueError:
+    pass
+
+  global_options = [
+#    make_option("-h", "--help", action="help"),
+    make_option("-v", "--verbose", action="store_true", dest="verbose"),
+    make_option("-q", "--quiet", action="store_false", dest="verbose"),
+    make_option("-r", "--dry-run", action="store_true", dest="readonly"),
+    make_option("-b", "--bugzilla", dest="bugzilla")
+  ]
+
+  global_defaults = {
+    "bugzilla" : os.environ['BUGZILLA'] if 'BUGZILLA' in os.environ else "bugzilla.mozilla.org"
+  }
+
+  commands = {
+    '' : BaseCommand(),
+    "help" : Help(),
+    "import" : Import(),
+  }
+
+  if command not in commands:
+    print "Unknown command %s" % command
+    print "Available commands are"
+    print ' '.join(commands.keys())
+    sys.exit()
+
+  global_options.extend(commands[command].options)
+  global_defaults.update(commands[command].defaults)
+
+  parser = OptionParser(commands[command].usage, option_list=global_options)
+  parser.set_defaults(**global_defaults)
+  options, positional_args = parser.parse_args(args=args)
+
+  commands[command].run()
