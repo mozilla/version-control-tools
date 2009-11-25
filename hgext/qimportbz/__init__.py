5710ac16b0c5e76be47ccea90b87b5998dafe29f
timeless@mozdev.org <timeless@mozdev.org>
2009-11-25 14:53:39 -0500

    ReSTifying help

--- hgext/qimportbz/__init__.py
+++ hgext/qimportbz/__init__.py
@@ -2,28 +2,29 @@
 
 Imports patches from bugzilla to your mercurial queue
 
-Configuration section is entirely optional but potentially useful.
+Configuration section is entirely optional but potentially useful::
 
-[qimportbz]
-bugzilla = server-address (defaults to BUGZILLA environment varable or bugzilla.mozilla.org)
-joinstr = string to join flags for commit messages (default is ' ')
-patch_format = Formatting string used to create patch names automatically
-msg_format = Formatting string used to create commit messages automatically
+  [qimportbz]
+  bugzilla = server-address (defaults to BUGZILLA environment varable or bugzilla.mozilla.org)
+  joinstr = string to join flags for commit messages (default is ' ')
+  patch_format = Formatting string used to create patch names automatically
+  msg_format = Formatting string used to create commit messages automatically
 
 Formatting strings are the standard python format strings where a dictionary is used to supply the data.
 For users of Mercurial 1.2: any % characters must be escaped since Python's configuration parser will try to interpret them.
 
-There are 5 pieces of patch metadata available for use:
+There are 5 pieces of patch metadata available for use::
 
-      "bugnum" : the bug number
-      "id" : the patch id (internal bugzilla number)
-      "title" : the bug title
-      "desc" : the patch description
-      "flags" : all the flags
+  "bugnum" : the bug number
+  "id" : the patch id (internal bugzilla number)
+  "title" : the bug title
+  "desc" : the patch description
+  "flags" : all the flags
 
-The default values are:
-patch_format = bug-%(bugnum)s
-msg_format = Bug %(bugnum)s - "%(title)s" [%(flags)s]
+The default values are::
+
+  patch_format = bug-%(bugnum)s
+  msg_format = Bug %(bugnum)s - "%(title)s" [%(flags)s]
 """
 from mercurial import commands, cmdutil, extensions, url, error
 from hgext import mq
