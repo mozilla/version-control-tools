c19c65d78213c43580b73a291a52024733fb83d4
Ted Mielczarek <ted.mielczarek@gmail.com>
2010-01-21 11:34:46 -0500

    add GPL license + header text

--- hghooks/mozhghooks/pushlog.py
+++ hghooks/mozhghooks/pushlog.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2010 Mozilla Foundation
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 # This script implements a Mercurial hook to log the date and time when
 # changesets are pushed into a repository, and the user that pushed them.
 #
