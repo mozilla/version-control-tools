10d3709c4530bb66724a61d4d417ac17a815c5ef
Ted Mielczarek <ted.mielczarek@gmail.com>
2010-03-09 11:28:47 -0500

    port all existing tests to the python harness

--- hghooks/runtests.py
+++ hghooks/runtests.py
@@ -6,6 +6,7 @@ from mercurial.commands import add, clone, commit, init, push
 from mercurial.node import hex
 from tempfile import mkdtemp
 import shutil
+import os, stat
 from os.path import join
 import sqlite3 as sqlite
 from getpass import getuser
@@ -32,18 +33,22 @@ def getPushesFromDB(repo):
                      'user':row[1],
                      'date':row[2],
                      'changes':[]})
+      s.add(row[0])
     pushes[-1]['changes'].append({'rev':row[3], 'node':row[4]})
   return pushes
 
 class TestHook(unittest.TestCase):
   def setUp(self):
+    self.ui = ui.ui()
+    self.ui.quiet = True
+    self.ui.verbose = False
     self.repodir = mkdtemp(prefix="hg-test")
-    init(ui.ui(), dest=self.repodir)
+    init(self.ui, dest=self.repodir)
     addHook(self.repodir)
-    self.repo = hg.repository(ui.ui(), self.repodir)
+    self.repo = hg.repository(self.ui, self.repodir)
     self.clonedir = mkdtemp(prefix="hg-test")
-    clone(ui.ui(), self.repo, self.clonedir)
-    self.clonerepo = hg.repository(ui.ui(), self.clonedir)
+    clone(self.ui, self.repo, self.clonedir)
+    self.clonerepo = hg.repository(self.ui, self.clonedir)
 
   def tearDown(self):
     shutil.rmtree(self.repodir)
@@ -51,7 +56,7 @@ class TestHook(unittest.TestCase):
 
   def testBasic(self):
     """Push one changeset, sanity check all the data."""
-    u = ui.ui()
+    u = self.ui
     appendFile(join(self.clonedir, "testfile"), "checkin 1")
     add(u, self.clonerepo, join(self.clonedir, "testfile"))
     commit(u, self.clonerepo, message="checkin 1 bug 12345")
@@ -69,6 +74,76 @@ class TestHook(unittest.TestCase):
     tip = self.clonerepo.changectx('tip')
     self.assertEqual(c['rev'], tip.rev())
     self.assertEqual(c['node'], hex(tip.node()))
+
+  def testTwoPushes(self):
+    """Push two changesets in two pushes, sanity check all the data."""
+    u = self.ui
+    appendFile(join(self.clonedir, "testfile"), "checkin 1")
+    add(u, self.clonerepo, join(self.clonedir, "testfile"))
+    commit(u, self.clonerepo, message="checkin 1 bug 12345")
+    push(u, self.clonerepo, dest=self.repodir)
+
+    appendFile(join(self.clonedir, "testfile"), "checkin 2")
+    commit(u, self.clonerepo, message="checkin 2 bug 23456")
+    push(u, self.clonerepo, dest=self.repodir)
+
+    pushes = getPushesFromDB(self.repo)
+    self.assertEqual(len(pushes), 2)
+    self.assert_(all(len(p['changes']) == 1 for p in pushes))
+    self.assert_(all(p['user'] == getuser() for p in pushes))
+    self.assert_(pushes[0]['date'] <= pushes[1]['date'])
+    self.assert_(pushes[0]['changes'][0]['rev'] <= pushes[1]['changes'][0]['rev'])
+    # check that all the node/rev pairs we recorded match what hg thinks
+    self.assert_(all(self.clonerepo.changectx(c['node']).rev() == c['rev'] for p in pushes for c in p['changes']))
+
+  def testTwoChangesOnePush(self):
+    """Push two changesets in one push, sanity check all the data."""
+    u = self.ui
+    appendFile(join(self.clonedir, "testfile"), "checkin 1")
+    add(u, self.clonerepo, join(self.clonedir, "testfile"))
+    commit(u, self.clonerepo, message="checkin 1 bug 12345")
+    appendFile(join(self.clonedir, "testfile"), "checkin 2")
+    commit(u, self.clonerepo, message="checkin 2 bug 23456")
+    start = time()
+    push(u, self.clonerepo, dest=self.repodir)
+    end = time()
+
+    pushes = getPushesFromDB(self.repo)
+    self.assertEqual(len(pushes), 1)
+    p = pushes[0]
+    self.assertEqual(len(p['changes']), 2)
+    self.assertEqual(p['user'], getuser())
+    self.assert_(p['date'] >= int(start))
+    self.assert_(p['date'] <= int(end))
+    self.assert_(p['changes'][0]['rev'] <= p['changes'][1]['rev'])
+    # check that all the node/rev pairs we recorded match what hg thinks
+    self.assert_(all(self.clonerepo.changectx(c['node']).rev() == c['rev'] for c in p['changes']))
+
+  def testPushlogPermissions(self):
+    """Check that the pushlog db is group writable after pushing."""
+    u = self.ui
+    appendFile(join(self.clonedir, "testfile"), "checkin 1")
+    add(u, self.clonerepo, join(self.clonedir, "testfile"))
+    commit(u, self.clonerepo, message="checkin 1 bug 12345")
+    push(u, self.clonerepo, dest=self.repodir)
+
+    st = os.stat(join(self.repodir, ".hg", "pushlog2.db"))
+    self.assertEqual(st.st_mode & stat.S_IWGRP, stat.S_IWGRP)
+
+  def testEmptyDB(self):
+    """bug 466149 - Check that pushing to a db without a schema succeeds."""
+    u = self.ui
+    # empty the db file
+    with open(join(self.repodir, ".hg", "pushlog2.db"), 'w') as f:
+      pass
+    appendFile(join(self.clonedir, "testfile"), "checkin 1")
+    add(u, self.clonerepo, join(self.clonedir, "testfile"))
+    commit(u, self.clonerepo, message="checkin 1 bug 12345")
+    push(u, self.clonerepo, dest=self.repodir)
+
+    p = getPushesFromDB(self.repo)
+    self.assertEqual(len(p), 1)
+    self.assertEqual(len(p[0]['changes']), 1)
 
 if __name__ == '__main__':
   unittest.main()
