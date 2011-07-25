f9833b54e2e61401bf136b654874422c7660dd01
Lukas Blakk <lsblakk@mozilla.com>
2011-07-25 16:17:38 -0400

    Bug 671289 update try hook output to indicate which change needs to have syntax in it p=lsblakk r=nthomas

--- hghooks/mozhghooks/try_mandatory.py
+++ hghooks/mozhghooks/try_mandatory.py
@@ -9,7 +9,7 @@ def hook(ui, repo, **kwargs):
     # Block the push unless they use the try_syntax
     # 'try: ' is enough to activate try_parser and get the default set
     if "try: " not in repo.changectx('tip').description():
-        print """To push to try you must use try syntax in your push comment 
+        print """To push to try you must use try syntax in the push comment of the *last* change 
 See %s to build your syntax
 For assistance using the syntax and %s for more information.
 Thank you for helping to reduce CPU cyles by asking for exactly what you need.""" % (chooserUrl, infoUrl)
