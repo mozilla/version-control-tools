48503905d5e724c46cce1a0091456a3fd47d718f
Noah Meyerhans <nmeyerhans@mozilla.com>
2011-08-15 15:19:14 -0700

    Implement the MirrorJob class for tracking push operations. Log output based on the exit code of a child.

--- hghooks/mozhghooks/mirror-daemon.py
+++ hghooks/mozhghooks/mirror-daemon.py
@@ -29,41 +29,59 @@ maxchildren = 3
 configfile = "/etc/mercurial/repo-mirrors.yaml"
 random.seed()
 
-# Spawn a child process for the given command
-# return subprocess.Popen object
-def spawn_child(cmd, verbose=False):
-    proc = Popen(shlex.split(cmd), stdout=PIPE, stderr=STDOUT)
-    if verbose:
-        print "Spawned [%s] as pid %i" % (cmd, proc.pid)
-    return proc
+class MirrorJob:
+    def __init__(self, host, path, config, fh, verbose=False):
+        self.host = host
+        self.path = path
+        self.child = None
+        self.command = self.make_command()
 
+    # Given a host and path to clone, return the command used to trigger a
+    # pull.
+    def make_command(self):
+        if self.config.has_key('daemon') and self.config['daemon'].has_key('ssh-id'):
+            id_str = "-i%s" % config['daemon']['ssh-id']
+        else:
+            id_str = ""
+        return "/usr/bin/ssh -n %s %s hg pull %s" % (id_str, self.host, self.url_path)
+
+    # Spawn a child process for the given command
+    def spawn_child(self):
+        self.child = Popen(shlex.split(self.command), stdout=PIPE, stderr=STDOUT)
+        if self.verbose:
+            print "Spawned [%s] as pid %i" % (self.command, self.proc.pid)
+        
 # Spawn subprocesses for each of the given commands, up to
 # max_children.  Return a list of subprocess.Popen objects
 def spawn_children(commands, max_children, verbose=False):
     procs = []
     while len(procs) < max_children and len(commands) > 0:
         p = spawn_child(commands[0], verbose)
+        commands[0].spawn_child()
+        procs.appends(commands[0])
         del commands[0]
-        procs.append(p)
     if verbose:
         print "Spawned %i processes, %i pending" % ( len(procs), len(commands) )
     return procs
 
 # check each child process, gather any output, 
-def reap_children(children, verbose=False):
+def reap_children(jobs, verbose=False):
     if verbose:
-        print "in reap_children, nchildren = %i" % len(children)
-    for child in children:
+        print "in reap_children, nchildren = %i" % len(jobs)
+    for job in jobs:
         output = None
-        rcode = child.poll()
+        rcode = job.child.poll()
         if(rcode != None):
-            output = child.communicate()
+            output = job.child.communicate()
+            if(rcode == 0):
+                print "Successfully pushed %s to %s" % (job.path, job.host)
+            else:
+                print "ERROR: Push of %s to %s returned %i" % (job.path, job.host, rcode)
             if verbose:
-                print "%i exited with code %i" % (child.pid, rcode)
                 if output:
                     print "Output: %s" % output[0]
-            children.remove(child)
-    return children
+            jobs.remove(child)
+    return jobs
 
 # Different repositories might get mirrored to different hosts.  For a
 # given repository, return a list of hosts that should receive push
@@ -73,16 +91,6 @@ def get_hosts_for_repo(repo, config):
     if(config.has_key(repo)):
         hosts = config[repo]['mirrors']
     return hosts
-
-# Given a host and path to clone, return the command used to trigger a
-# pull. config object and trigger filehandle are also available to
-# this function.
-def make_command(host, url_path, config, fh):
-    if config.has_key('daemon') and config['daemon'].has_key('ssh-id'):
-        id_str = "-i%s" % config['daemon']['ssh-id']
-    else:
-        id_str = ""
-    return "/usr/bin/ssh -n %s %s hg pull %s" % (id_str, host, url_path)
 
 # Look for repositories that have been updated. Return a list of
 # commands to run to notify the appropriate mirrors that they should
@@ -100,10 +108,10 @@ def get_more_commands(directory, config, verbose=False):
             os.unlink(fullpath)
             unlock(fullpath, lck, verbose)
             for host in get_hosts_for_repo(urllib.unquote(f), config):
-                cmnds.append(make_command(host, 
-                                          urllib.unquote(f), 
-                                          config,
-                                          fh))
+                cmnds.append(MirrorJob(host, 
+                                       urllib.unquote(f), 
+                                       config,
+                                       fh))
                 if verbose:
                     print "Appended a command to the queue. ",
                     print "qlen: %i" % len(cmnds)
@@ -126,17 +134,17 @@ def read_config():
     return y
 
 def main():
-    verbose  = True
-    children = []
-    commands = []
+    verbose      = True
+    running_jobs = []
+    pending_jobs = []
     dir = "/dev/shm/hg_pushes"
     cfg = read_config()
     while True:
-        commands = commands + get_more_commands(dir, cfg, verbose)
-        children = reap_children(children, verbose)
-        children = children + spawn_children(commands, 
-                                  maxchildren - len(children), 
-                                  verbose)
+        pending_jobs = pending_jobs + get_more_commands(dir, cfg, verbose)
+        running_jobs = reap_children(running_jobs, verbose)
+        running_jobs = running_jobs + spawn_children(pending_jobs, 
+                                                     maxchildren - len(running_jobs), 
+                                                     verbose)
         sleep(1)
 
 if __name__ == "__main__":
