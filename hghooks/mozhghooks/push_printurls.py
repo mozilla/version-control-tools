08b99bfab2192c44329329619938f5332ef30914
Graeme McCutcheon <graememcc_firefox@graeme-online.co.uk>
2012-04-27 16:38:03 +0100

    Changegroup hook for printing URLs of changesets in push. r=ted

---
+++ hghooks/mozhghooks/push_printurls.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+import os.path
+from mercurial.node import short
+
+hgNameToRevURL = {
+    'comm-central'    : 'comm-central/',
+    'fx-team'         : 'integration/fx-team/',
+    'mozilla-central' : 'mozilla-central/',
+    'mozilla-inbound' : 'integration/mozilla-inbound/',
+    'try'             : 'try/',
+}
+
+
+def hook(ui, repo, node, hooktype, **kwargs):
+    repo_name = os.path.basename(repo.root)
+    if repo_name not in hgNameToRevURL:
+        return 0
+
+    # All changesets from node to "tip" inclusive are part of this push.
+    rev = repo.changectx(node).rev()
+    tip = repo.changectx('tip').rev()
+
+    num_changes = tip + 1 - rev
+    url = 'https://hg.mozilla.org/' + hgNameToRevURL[repo_name]
+
+    if num_changes <= 10:
+        plural = 's' if num_changes > 1 else ''
+        print 'You can view your change%s at the following URL%s:' % (plural, plural)
+
+        for i in xrange(rev, tip + 1):
+            node = short(repo.changectx(i).node())
+            print '  %srev/%s' % (url, node)
+    else:
+       tip_node = short(repo.changectx(tip).node())
+       print 'You can view the pushlog for your changes at the following URL:'
+       print '  %spushloghtml?changeset=%s' % (url, tip_node)
+
+    return 0
