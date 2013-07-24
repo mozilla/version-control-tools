ceecc39cf48671936a11aace4235c90191e27bd6
Gregory Szorc <gps@mozilla.com>
2013-07-24 09:36:21 -0700

    Use GPL license because that's what Mercurial extensions require

--- hgext/mozext/__init__.py
+++ hgext/mozext/__init__.py
@@ -1,6 +1,5 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2 or any later version.
 
 """make Mozilla contributors more productive
 
