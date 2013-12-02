3a3f6c539dfd90e28364e2d8aff06877b3d43648
Dustin J. Mitchell <dustin@mozilla.com>
2013-12-02 14:02:16 -0500

    Bug 917485: limit the set of people who can make changes to PuppetAgain; r=gps f=fubar

---
+++ hghooks/mozhghooks/whitelist_releng.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2010 Mozilla Foundation
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+# This script implements a whitelist containing people from the releng and relops teams.
+#
+# run `python setup.py install` to install the module in the proper place,
+# and then modify the repository's hgrc as per example-hgrc.
+
+import os
+
+ALLOWED_USERS = set([
+    'armenzg@mozilla.com',
+    'arich@mozilla.com',
+    'asasaki@mozilla.com',
+    'bhearsum@mozilla.com',
+    'catlee@mozilla.com',
+    'coop@mozilla.com',
+    'dustin@mozilla.com',
+    'hwine@mozilla.com',
+    'jarmstrong@mozilla.com',
+    'jhopkins@mozilla.com',
+    'jlund@mozilla.com',
+    'joduinn@mozilla.com',
+    'jwatkins@mozilla.com',
+    'jwood@mozilla.com',
+    'kmoir@mozilla.com',
+    'mcornmesser@mozilla.com',
+    'mshal@mozilla.com',
+    'mtabara@mozilla.com',
+    'nthomas@mozilla.com',
+    'pmoore@mozilla.com',
+    'qfortier@mozilla.com',
+    'raliiev@mozilla.com',
+    'sbruno@mozilla.com',
+])
+
+def hook(ui, repo, node=None, **kwargs):
+    rev = repo[node].rev()
+    tip = repo['tip'].rev()
+    branches = set(repo.changectx(i).branch() for i in range(rev, tip+1))
+    if 'production' in branches and os.environ['USER'] not in ALLOWED_USERS:
+        print "** you (%s) are not allowed to push to the production branch" % (os.environ['USER'],)
+        return 1
+    return 0
