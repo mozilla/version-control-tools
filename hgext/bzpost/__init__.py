a84c8461db37e7c66e8afde30087b79d418c9095
Gregory Szorc <gps@mozilla.com>
2014-07-20 22:24:45 -0500

    bzpost: mark compatible with 3.0

--- hgext/bzpost/__init__.py
+++ hgext/bzpost/__init__.py
@@ -60,7 +60,7 @@ from mozautomation.repository import (
 )
 from mozhg.auth import getbugzillaauth
 
-testedwith = '3.0.1 3.0.2'
+testedwith = '3.0 3.0.1 3.0.2'
 
 def wrappedpushbookmark(orig, pushop):
     result = orig(pushop)
