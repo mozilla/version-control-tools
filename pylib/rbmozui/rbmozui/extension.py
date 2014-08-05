9d0262db8ac4f48160338f855406cd86743d9653
Mike Conley <mconley@mozilla.com>
2014-08-05 19:52:22 -0400

    Backing out c05d15ed4bea.

--- pylib/rbmozui/rbmozui/extension.py
+++ pylib/rbmozui/rbmozui/extension.py
@@ -20,9 +20,6 @@ class RBMozUI(Extension):
         'Summary': 'UI tweaks to Review Board for Mozilla',
     }
     css_bundles = {
-        'commits': {
-            'source_filenames': ['css/commits.less'],
-        },
         'default': {
             'source_filenames': ['css/common.css'],
         },
@@ -36,9 +33,6 @@ class RBMozUI(Extension):
     js_bundles = {
         'commits': {
             'source_filenames': ['js/commits.js'],
-        },
-        'review': {
-            'source_filenames': ['js/review.js'],
         }
     }
 
@@ -51,18 +45,16 @@ class RBMozUI(Extension):
           fieldset.remove_field(field)
         # All of our review request styling is injected via review-stylings-css,
         # which in turn loads the review.css static bundle.
-        TemplateHook(self, 'base-css', 'rbmozui/commits-stylings-css.html',
-                     apply_to='rbmozui-commits')
         TemplateHook(self, 'base-css', 'rbmozui/review-stylings-css.html',
-                     apply_to=review_request_url_names + ['rbmozui-commits'])
+                     apply_to=review_request_url_names)
         TemplateHook(self, 'base-css', 'rbmozui/viewdiff-stylings-css.html',
                      apply_to=diffviewer_url_names)
         TemplateHook(self, 'base-before-content', 'rbmozui/review-header.html',
-                     apply_to=review_request_url_names + ['rbmozui-commits'])
-        TemplateHook(self, 'base-scripts-post', 'rbmozui/review-js.html',
+                     apply_to=review_request_url_names)
+        TemplateHook(self, 'base-scripts-post', 'rbmozui/review-header-workaround.html',
                      apply_to=review_request_url_names)
         TemplateHook(self, 'rbmozui-commits-scripts', 'rbmozui/commits-scripts-js.html',
-                     apply_to='rbmozui-commits')
+                     apply_to="rbmozui-commits")
         urlpatterns = patterns('',
             url(r'^rbmozui/', include('rbmozui.urls')))
         URLHook(self, urlpatterns)
--- pylib/rbmozui/rbmozui/static/js/commits.js
+++ pylib/rbmozui/rbmozui/static/js/commits.js
@@ -1,125 +1,10 @@
 var RBMozUI = RBMozUI || {};
 
-$(document).ready(function() {
+(function() {
 
-  var $ErrorContainer = $("#error-container");
-  function reportError(aMsg) {
-    // A pretty terrible error reporting mechanism, but it sure
-    // beats modal alert dialogs.
-    $("#error-info").text(aMsg);
-    $ErrorContainer.attr("haserror", "true");;
-  }
-
-  $("#error-close").click(function(aEvent) {
-    $ErrorContainer.attr("haserror", false);
-  });
-
-  RBMozUI.Reviewer = RB.BaseResource.extend({
-    defaults: {
-      username: ""
-    },
-
-    initialize: function(aOptions) {
-      var person = aOptions.person;
-      if (person.hasOwnProperty('username')) {
-        this.set('username', person['username']);
-      } else {
-        this.set('username', person['title']);
-      }
-    }
-  });
-
-  RBMozUI.ReviewerList = Backbone.Collection.extend({
-    model: RBMozUI.Reviewer
-  });
-
-  RBMozUI.Commit = Backbone.Model.extend({
-    defaults: {
-      loaded: false,
-    },
-
-    requestOrDraft: function() {
-      return this.reviewRequest.draft.id !== undefined ?
-             this.reviewRequest.draft :
-             this.reviewRequest;
-    },
-
-    hasDraft: function() {
-      return this.reviewRequest.draft.id !== undefined;
-    },
-
-    initialize: function(options) {
-      this.reviewers = new RBMozUI.ReviewerList();
-      this.reviewRequest = new RB.ReviewRequest({
-        id: options.reviewRequestID,
-      });
-      this.commitID = options.commitID;
-      this.commitNum = options.commitNum;
-      this.editor = new RB.ReviewRequestEditor({
-        reviewRequest: this.reviewRequest
-      });
-    },
-
-    ready: function(aSuccess, aFailure) {
-      this.reviewRequest.draft.ready({
-        ready: function() {
-          aSuccess();
-          this.resetReviewers();
-          this.set("loaded", true);
-        },
-        error: function(aMessage) {
-          aFailure(aMessage);
-        }
-      }, this);
-    },
-
-    resetReviewers: function() {
-      var targetPeople = this.requestOrDraft().get('targetPeople');
-      var reviewers = _.map(targetPeople, function(aTargetPerson) {
-        return new RBMozUI.Reviewer({person: aTargetPerson});
-      });
-      this.reviewers.reset(reviewers);
-    },
-
-    removeReviewer: function(aUsername) {
-      var usernames = this.reviewers
-                          .pluck('username')
-                          .filter(function(username) {
-                            return username != aUsername;
-                          }).join(',');
-      var self = this;
-      this.editor.setDraftField('targetPeople', usernames, {
-        jsonFieldName: 'target_people',
-        success: function() {
-          console.log('Successfully removed reviewer ' + aUsername);
-          self.resetReviewers();
-        },
-        error: function(aErrorObject) {
-          console.error(aErrorObject.errorText);
-        }
-      });
-    },
-
-    addReviewer: function(aUsername) {
-      var usernames = this.reviewers.pluck('username');
-      usernames.push(aUsername);
-
-      var usernameString = usernames.join(',');
-      var self = this;
-
-      this.editor.setDraftField('targetPeople', usernameString, {
-        jsonFieldName: 'target_people',
-        success: function() {
-          console.log('Successfully added reviewer ' + aUsername);
-          self.resetReviewers();
-        },
-        error: function(aErrorObject) {
-          reportError(aErrorObject.errorText);
-        }
-      });
-    }
-  })
-
+  var kIsP2RB = "p2rb";
+  var kIsSquashed = "p2rb.is_squashed";
+  var kCommits = "p2rb.commits";
 
   RBMozUI.CommitsList = Backbone.Collection.extend({
     model: RB.ReviewRequest
@@ -129,259 +14,55 @@ $(document).ready(function() {
 
   RBMozUI.CommitView = Backbone.View.extend({
     tagName: 'li',
-    className: 'commit-list-item',
-    events: {
-      'click .remove': 'removeReviewer',
-      'submit .reviewer-form': 'onReviewerSubmit'
-    },
-    loadingTemplate: _.template($('#loading-template').html()),
-    commitTemplate: _.template($('#commit-template').html()),
-    reviewerTemplate: _.template($('#reviewer-template').html()),
-
-    initialize: function(aOptions) {
-      this.listenTo(this.model, "change", this.render);
-      this.listenTo(this.model.reviewRequest, "change", this.render);
-      this.listenTo(this.model.reviewRequest.draft, "change", this.render);
-      this.listenTo(this.model.reviewers, "reset", this.reviewersChanged);
-      // Kick off loading the Commit that we're holding onto.
-      this.model.ready(function() {
-        console.log('Successfully loaded Commit with ID' + this.model.commitID);
-      }.bind(this), this.handleError.bind(this));
-    },
-
-    handleError: function(aErrorMessage) {
-      reportError(aErrorMessage);
-    },
-
-    computeState: function() {
-      if (!this.model.reviewRequest.get('public')) {
-        return "needs-publishing";
-      }
-
-      if (this.model.hasDraft()) {
-        return "unsaved-changes";
-      }
-
-      return "published";
-    },
-
-    reviewersChanged: function() {
-      this.render();
-      this.$el.find('input.reviewer-input').focus();
-    },
-
     render: function() {
-      // Hack alert - if an autocomplete GET request is still making the round
-      // trip by the time we re-render (if the user, for example, new the
-      // email address and just typed it in and pressed enter), in order to
-      // prevent the autocomplete popup from showing up anyways (and anchoring
-      // to the top left corner of the screen since the .reviewer-input is about
-      // to be replaced), we blur the input so that rbautocomplete ignores any
-      // requests coming in for it until it's refocused.
-      var input = this.$el.find('input.reviewer-input');
-      if (input) {
-        input.blur();
+      if (!this.template) {
+        this.template = _.template($('#commit-template').html());
       }
-
-      // In the base case, we've just kicked off the request to populate
-      // our review request and draft models, so we show a loading spinner.
-      if (!this.model.get("loaded")) {
-        this.$el.html(this.loadingTemplate({
-          commitID: this.model.commitID
-        }));
-        return this;
-      }
-
-      var reviewable = this.model.requestOrDraft();
-      var commitLink = SITE_ROOT + 'r/' + this.model.reviewRequest.id;
-
-      this.$el.html(this.commitTemplate({
-        reviewerTemplate: this.reviewerTemplate,
-        commitLink: commitLink,
-        commitNum: this.model.commitNum,
-        summary: reviewable.get('summary'),
-        description: reviewable.get('description'),
-        hasReviewers: reviewable.get('targetPeople').length > 0,
-        reviewers: this.model.reviewers,
-        commitID: this.model.commitID,
-        state: this.computeState()
+      this.$el.html(this.template({
+        summary: this.model.get('summary')
       }));
-
-      this.buildReviewerAutocomplete(
-        this.$el.find('input.reviewer-input')
-      );
       return this;
-    },
-
-    buildReviewerAutocomplete: function($elArray) {
-      if (!$elArray.length) {
-        reportError("Couldn't find reviewer input for commit " + this.model.commitID);
-      }
-      var reviewRequest = this.model.reviewRequest;
-      var $el = $($elArray[0]);
-      var options = {
-        fieldName: 'users',
-        nameKey: 'username',
-        descKey: 'fullname',
-        extraParams: {
-          fullname: 1
-        },
-        cmp: function(term, a, b) {
-          /*
-           * Sort the results with username matches first (in
-           * alphabetical order), followed by real name matches (in
-           * alphabetical order)
-           */
-          var aUsername = a.data.username,
-              bUsername = b.data.username,
-              aFullname = a.data.fullname,
-              bFullname = a.data.fullname;
-
-          if (aUsername.indexOf(term) === 0) {
-              if (bUsername.indexOf(term) === 0) {
-                  return aUsername.localeCompare(bUsername);
-              }
-              return -1;
-          } else if (bUsername.indexOf(term) === 0) {
-              return 1;
-          } else {
-              return aFullname.localeCompare(bFullname);
-          }
-        }
-      };
-
-      $el.rbautocomplete({
-        formatItem: function(data) {
-            var s = data[options.nameKey];
-
-            if (options.descKey && data[options.descKey]) {
-                s += ' <span>(' + _.escape(data[options.descKey]) +
-                     ')</span>';
-            }
-
-            return s;
-        },
-        matchCase: false,
-        multiple: false,
-        parse: function(data) {
-            var items = data[options.fieldName],
-                itemsLen = items.length,
-                parsed = [],
-                value,
-                i;
-
-            for (i = 0; i < itemsLen; i++) {
-                value = items[i];
-
-                parsed.push({
-                    data: value,
-                    value: value[options.nameKey],
-                    result: value[options.nameKey]
-                });
-            }
-
-            return parsed;
-        },
-        url: SITE_ROOT +
-             'api/' + (options.resourceName || options.fieldName) + '/',
-        extraParams: options.extraParams,
-        cmp: options.cmp,
-        width: 350,
-        error: function(xhr) {
-            var text;
-
-            try {
-                text = $.parseJSON(xhr.responseText).err.msg;
-            } catch (e) {
-                text = 'HTTP ' + xhr.status + ' ' + xhr.statusText;
-            }
-
-            reportError(text);
-        }
-      }).on('autocompleteshow', function() {
-        /*
-         * Add the footer to the bottom of the results pane the
-         * first time it's created.
-         *
-         * Note that we may have multiple .ui-autocomplete-results
-         * elements, and we don't necessarily know which is tied to
-         * this. So, we'll look for all instances that don't contain
-         * a footer.
-         */
-        var resultsPane = $('.ui-autocomplete-results:not(' +
-                            ':has(.ui-autocomplete-footer))');
-
-        if (resultsPane.length > 0) {
-            $('<div/>')
-                .addClass('ui-autocomplete-footer')
-                .text(gettext('Press Tab to auto-complete.'))
-                .appendTo(resultsPane);
-        }
-      });
-    },
-
-    removeReviewer: function(aEvent) {
-      var username = aEvent.target.dataset.username;
-      console.log('Removing reviewer with id: ' + username);
-      this.model.removeReviewer(username);
-    },
-
-    onReviewerSubmit: function(aEvent) {
-      aEvent.preventDefault();
-      var username = this.$el.find('.reviewer-input').first().val();
-      console.log('Adding reviewer with id: ' + username);
-      this.model.addReviewer(username);
     }
   });
 
   RBMozUI.CommitsView = Backbone.View.extend({
-    initialize: function(aOptions) {
-      this.listenTo(Commits, 'add', this.add);
-      this.model = new RB.ReviewRequest({id: aOptions.squashedID});
+    initialize: function() {
 
-      $("#publish").click(this.publish.bind(this));
+      this.listenTo(Commits, 'add', this.addOne);
 
-      var commits = aOptions.commits;
-      var commitModels = commits.map(function(aTuple, aIndex) {
-        return new RBMozUI.Commit({
-          reviewRequestID: aTuple[1],
-          commitNum: aIndex,
-          commitID: aTuple[0]
-        });
-      });
-      Commits.add(commitModels);
-    },
-
-    add: function(aCommit) {
-      var view = new RBMozUI.CommitView({model: aCommit});
-      // At this point, we don't have to worry about commit
-      // removal, so let's just add this and forget about it.
-      $("#commit-list").append(view.render().el);
-    },
-
-    publish: function() {
-      $("#publish").prop("disabled", true);
-      this.model.draft.ready({
+      this.squashed = new RB.ReviewRequest({id: this.id});
+      var self = this;
+      this.squashed.ready({
         ready: function() {
-          var editor = new RB.ReviewRequestEditor({reviewRequest: this.model});
-          editor.setDraftField('public', true, {
-            jsonFieldName: 'public',
-            success: function() {
-              console.log('Successfully published - reloading page...');
-              $("#publish").prop("disabled", false);
-              location.reload();
-            },
-            error: function(aMsg) {
-              $("#publish").prop("disabled", false);
-              reportError(aMsg);
-            }
-          })
+          var extraData = self.squashed.get('extraData');
+          if (!extraData[kIsP2RB]) {
+            // Do something reasonable here.
+            console.error("This review request (id: " + self.id + ") does not appear to be a p2rb push.");
+            return;
+          }
+          if (!extraData[kIsSquashed] == "True") {
+            // Do something reasonable here.
+            console.error("This review request (id: " + self.id + ") does not appear to be a squashed review.");
+            return;
+          }
+
+          var commits = JSON.parse(extraData[kCommits]);
+          var commitModels = commits.map(function(aTuple) {
+            return new RB.ReviewRequest({id: aTuple[1]});
+          });
+          Commits.add(commitModels);
         }
-      }, this);
+      });
+    },
+
+    addOne: function(aCommit) {
+      aCommit.ready({
+        ready: function() {
+          var view = new RBMozUI.CommitView({model: aCommit});
+          this.$("#commit-list").append(view.render().el);
+        }
+      })
     }
   });
 
-  // I'll bet you're wondering what kicks this all off. Well, that'd be
-  // in the commits.html template, which calls into RBMozUI and gets all
-  // of this machinery working.
-});
+})();
