8265d572a0dbcdebe15516f1f70380c446747e1a
Mike Conley <mconley@mozilla.com>
2014-09-02 22:37:04 -0400

    rbmozui: Bug 1058786 - Part 1: Remove TargetPeople field from review request. r=mcote.
    --HG--
    extra : rebase_source : 939b1e2e6aa19dc1f7bba0913c1015d29016090e
    extra : histedit_source : 5edbf19d7d0611083beb18dc2cde5a6a513df07e%2C094caba6500d8ad79c6a9347a62fbb2a7606bea6

--- pylib/rbmozui/rbmozui/extension.py
+++ pylib/rbmozui/rbmozui/extension.py
@@ -7,7 +7,8 @@ from django.conf.urls import include, patterns, url
 
 from reviewboard.extensions.base import Extension
 from reviewboard.extensions.hooks import (TemplateHook, URLHook)
-from reviewboard.reviews.builtin_fields import TestingDoneField
+from reviewboard.reviews.builtin_fields import (TargetPeopleField,
+                                                TestingDoneField)
 from reviewboard.reviews.fields import (get_review_request_field,
                                         get_review_request_fieldset)
 from reviewboard.urls import (diffviewer_url_names,
@@ -45,10 +46,16 @@ class RBMozUI(Extension):
     def initialize(self):
         # Start by hiding the Testing Done field in all review requests, since
         # Mozilla developers will not be using it.
-        fieldset = get_review_request_fieldset('main')
-        field = get_review_request_field('testing_done')
-        if (field):
-          fieldset.remove_field(field)
+        main_fieldset = get_review_request_fieldset('main')
+        testing_done_field = get_review_request_field('testing_done')
+        if testing_done_field:
+            main_fieldset.remove_field(testing_done_field)
+
+        reviewers_fieldset = get_review_request_fieldset('reviewers')
+        people_field = get_review_request_field('target_people')
+        if people_field:
+            reviewers_fieldset.remove_field(people_field)
+
         # All of our review request styling is injected via review-stylings-css,
         # which in turn loads the review.css static bundle.
         TemplateHook(self, 'base-css', 'rbmozui/commits-stylings-css.html',
@@ -70,8 +77,14 @@ class RBMozUI(Extension):
     def shutdown(self):
         # We have to put the TestingDone field back before we shut down
         # in order to get the instance back to its original state.
-        fieldset = get_review_request_fieldset('main')
-        field = get_review_request_field('testing_done')
-        if not field:
-          fieldset.add_field(TestingDoneField)
+        main_fieldset = get_review_request_fieldset('main')
+        testing_done_field = get_review_request_field('testing_done')
+        if not testing_done_field:
+            main_fieldset.add_field(TestingDoneField)
+
+        reviewers_fieldset = get_review_request_fieldset('reviewers')
+        people_field = get_review_request_field('target_people')
+        if not people_field:
+            reviewers_fieldset.add_field(TargetPeopleField)
+
         super(RBMozUI, self).shutdown()
