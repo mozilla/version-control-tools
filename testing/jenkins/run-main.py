6d110281f012378df3f975473160dce7f67ffd08
Gregory Szorc <gps@mozilla.com>
2014-10-03 14:44:39 -0700

    jenkins: blow away vagrant vm when last build was bad
    --HG--
    extra : rebase_source : 83fa5077c1d5e73754427f76821675084bd52a3d

--- testing/jenkins/run-main.py
+++ testing/jenkins/run-main.py
@@ -22,12 +22,36 @@ if not VAGRANT:
     print('vagrant not found')
     sys.exit(1)
 
+if 'BUILD_NUMBER' not in os.environ:
+    print('BUILD_NUMBER not defined in environment. Are you running '
+            'inside Jenkins?')
+    sys.exit(1)
+
 CWD = os.getcwd()
+
+LAST_BUILD_PATH = os.path.join(CWD, 'last-good-build')
+BUILD_NUMBER = int(os.environ['BUILD_NUMBER'])
+
+last_good_build = None
+if os.path.exists(LAST_BUILD_PATH):
+    with open(LAST_BUILD_PATH, 'rb') as fh:
+        last_good_build = int(fh.read().strip())
+
+# If the last build failed, we destroy our Vagrant environment and rebuild from
+# scratch in case things are in an inconsistent state. This is a bit hacky,
+# but is the easiest solution to a complicated problem.
+if last_good_build is None or BUILD_NUMBER > last_good_build + 1:
+    print('Last build was bad. Blowing away virtual machine just in case.')
+    subprocess.check_call([VAGRANT, 'destroy', '-f'], cwd=HERE)
 
 subprocess.check_call([VAGRANT, 'up'], cwd=HERE)
 
 res = subprocess.call([VAGRANT, 'ssh', '--', '/version-control-tools/testing/jenkins/run.sh'],
     cwd=HERE)
+
+if res == 0:
+    with open(LAST_BUILD_PATH, 'wb') as fh:
+        fh.write('%d' % BUILD_NUMBER)
 
 subprocess.check_call([VAGRANT, 'halt'], cwd=HERE)
 
