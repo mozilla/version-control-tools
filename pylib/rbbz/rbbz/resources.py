908c91dc931e68278e6df1912c6f829ef1c9523c
Steven MacLeod <smacleod@mozilla.com>
2014-10-16 10:37:48 -0400

    rbbz: Add missing license header to resources.py

--- pylib/rbbz/rbbz/resources.py
+++ pylib/rbbz/rbbz/resources.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 from django.contrib.auth import login
 from djblets.webapi.decorators import (webapi_request_fields,
                                        webapi_response_errors)
