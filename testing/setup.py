cb5df38a59db6077de035f341dd69600da5328bb
Gregory Szorc <gps@mozilla.com>
2014-10-28 11:43:34 -0700

    testing: add vcttesting to virtualenv
    The support modules in the vcttesting package are becoming increasingly
    popular. This directory currently must be manually added to sys.path.
    This is annoying.
    
    This patch adds packaging to vcttesting so it is available in our
    virtualenv.
    
    Scripts manually adding the testing directory to sys.path have been
    updated to no longer do this.
    
    --HG--
    extra : rebase_source : 79bb7b3bd7954b5d8576c4d5b81bd9bac2dba08e
    extra : amend_source : 1e4111d8d2208186865986569234fe6633dc9f38
    extra : histedit_source : 77f9d59b0cd2ccffdbb4749d35da992887b6ee59

---
+++ testing/setup.py
@@ -0,0 +1,13 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from setuptools import setup
+
+setup(
+    name='vcttesting',
+    version='0.0.1',
+    description='Support code to support testing the version-control-tools repo',
+    license='MPL',
+    packages=['vcttesting'],
+)
