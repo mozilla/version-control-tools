17670837bcb16697ac49a52af089513bdb3f8cda
Gregory Szorc <gps@mozilla.com>
2014-11-18 19:46:14 -0800

    serverlog: parse SSH sessions

--- hgext/serverlog/scripts/parse.py
+++ hgext/serverlog/scripts/parse.py
@@ -25,8 +25,48 @@ class Request(object):
         self.cpu_time = None
         self.end_date = None
 
+
+class SSHSession(object):
+    """Represents a full SSH session."""
+    def __init__(self, sid, date, repo, username):
+        self.id = sid
+        self.start_date = date
+        self.repo = repo
+        self.username = username
+        self.commands = []
+        self.current_command = None
+        self.wall_time = None
+        self.cpu_time = None
+        self.end_date = None
+
+    @property
+    def session_type(self):
+        """Determine the type of the session.
+
+        The following types exist:
+
+          push - New changesets were pushed
+          pushkey - New keys were set but no changesets were pushed
+          pull - Changesets were retrieved
+          lookup - Simple repository lookup. Most likely a no-op push or pull.
+        """
+        commands = set([t[0] for t in self.commands])
+
+        if 'unbundle' in commands:
+            return 'push'
+
+        if 'pushkey' in commands:
+            return 'pushkey'
+
+        if 'getbundle' in commands:
+            return 'pull'
+
+        return 'lookup'
+
+
 def parse_events(fh):
     requests = {}
+    sessions = {}
 
     for line in fh:
         date = None
@@ -81,19 +121,48 @@ def parse_events(fh):
 
         elif action == 'BEGIN_SSH_SESSION':
             repo, username = parts[2:]
+            assert session
+            sessions[session] = SSHSession(session, date, repo, username)
 
         elif action == 'END_SSH_SESSION':
             t_wall, t_cpu = parts[2:]
             t_wall = float(t_wall)
             t_cpu = float(t_cpu)
 
+            s = sessions.get(session)
+            if not s:
+                continue
+
+            s.end_date = date
+            s.wall_time = t_wall
+            s.cpu_time = t_cpu
+
+            del sessions[session]
+            yield s
+
         elif action == 'BEGIN_SSH_COMMAND':
-            command = parts[2:]
+            command = parts[2]
+            s = sessions.get(session)
+            if s:
+                s.current_command = (request, command, date)
 
         elif action == 'END_SSH_COMMAND':
             t_wall, t_cpu = parts[2:]
             t_wall = float(t_wall)
             t_cpu = float(t_cpu)
+
+            s = sessions.get(session)
+            if not s:
+                continue
+
+            current = s.current_command
+            if current and current[0] == request:
+                s.commands.append((current[1], current[2], date, t_wall, t_cpu))
+
+            # The request IDs don't match or there is no beginning event.
+            # This is weird. We just don't record the command.
+
+            s.current_command = None
 
         elif action == 'CHANGEGROUPSUBSET_START':
             source, count = parts[2:]
@@ -104,14 +173,23 @@ def parse_events(fh):
             count = int(count)
 
 def print_stream(fh):
-    for r in parse_events(fh):
-        if r.start_date:
-            d = r.start_date.isoformat()
-        else:
-            d = 'None'
-        print('%s %s %s %s %s %s %s' % (d, r.repo, r.ip, r.command,
-            r.write_count, r.wall_time, r.cpu_time))
-
+    for thing in parse_events(fh):
+        if isinstance(thing, Request):
+            r = thing
+            if r.start_date:
+                d = r.start_date.isoformat()
+            else:
+                d = 'None'
+            print('%s %s %s %s %s %s %s' % (d, r.repo, r.ip, r.command,
+                r.write_count, r.wall_time, r.cpu_time))
+        elif isinstance(thing, SSHSession):
+            s = thing
+            if s.start_date:
+                d = s.start_date.isoformat()
+            else:
+                d = 'None'
+            print('%s %s %s %s %s %s %s' % (d, s.id, s.repo, s.session_type,
+                s.username, s.wall_time, s.cpu_time))
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
