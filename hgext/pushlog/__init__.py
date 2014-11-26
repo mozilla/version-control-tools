7ea3d574b4724a86578a0f98e8a984340297d1f7
Gregory Szorc <gps@mozilla.com>
2014-11-26 13:40:01 -0800

    pushlog: support pulling pushlog data over wire protocol; r=glandium
    This patch introduces a "pushlog" wire protocol command that returns
    information on pushes.
    
    The pull operation is overloaded in the client to fetch this pushlog
    data, if available. The fetched data will be added to the local pushlog
    database as if a push occurred to the local machine.
    
    Currently, the pushlog pulling assumes a unidirectional flow with a
    single source of truth. It thus only really works for master-slave
    replication. Pushlog data is generally useful and giving clients
    access to pushlog data (possibly via the same extension) is a noble
    goal. However, it is not an immediate goal.
    
    We should consider having a separate mode for "advanced" pushlog use
    cases (including multi remote) or we should consider splitting the
    pushlog extension into different extensions, each optimized for a
    particular use case.
    
    --HG--
    extra : rebase_source : 20cbe3e2e9256ecfed56e3d045083dcf668b4521
    extra : amend_source : 9735cfd3d30697d94ff9c4049b80225ebdd5f1dd

--- hgext/pushlog/__init__.py
+++ hgext/pushlog/__init__.py
@@ -10,6 +10,15 @@ import stat
 import time
 import weakref
 
+from mercurial import (
+    exchange,
+    extensions,
+    wireproto,
+)
+
+from mercurial.error import Abort
+from mercurial.node import bin, hex
+
 testedwith = '3.0 3.1 3.2'
 
 SCHEMA = [
@@ -21,6 +30,78 @@ SCHEMA = [
     'CREATE INDEX IF NOT EXISTS pushlog_date ON pushlog (date)',
     'CREATE INDEX IF NOT EXISTS pushlog_user ON pushlog (user)',
 ]
+
+# Wraps capabilities wireproto command to advertise pushlog availability.
+def capabilities(orig, repo, proto):
+    caps = orig(repo, proto)
+    caps.append('pushlog')
+    return caps
+
+@wireproto.wireprotocommand('pushlog', 'firstpush')
+def pushlogwireproto(repo, proto, firstpush):
+    """Return pushlog data from a start offset.
+
+    The response is a series of lines.
+
+    The first line starts with "0" or "1". This indicates success. If the line
+    starts with "0", an error message should follow. e.g.
+    ``0 exception when accessing sqlite``.
+
+    All subsequent lines describe individual pushes. The format of these lines
+    is:
+
+      <push ID> <author> <when> <node0> [<node1> [<node2> ...]]
+
+    That is:
+
+      * An integer ID for the push.
+      * A string of the user (SSH username) who performed the push
+      * Integer seconds since UNIX epoch when this push was performed
+      * A list of 40 byte hex changesets included in the push, in revset order
+    """
+    lines = ['1']
+
+    try:
+        firstpush = int(firstpush)
+
+        for pushid, who, when, nodes in repo.pushlog.pushes():
+            if pushid < firstpush:
+                continue
+
+            lines.append('%d %s %d %s' % (pushid, who, when, ' '.join(nodes)))
+
+        return '\n'.join(lines)
+    except Exception as e:
+        return '\n'.join(['0', str(e)])
+
+def exchangepullpushlog(orig, pullop):
+    """This is called during pull to fetch pushlog data."""
+    # check stepsdone for future compatibility with bundle2 pushlog exchange.
+    if 'pushlog' in pullop.stepsdone or not pullop.remote.capable('pushlog'):
+        return orig(pullop)
+
+    repo = pullop.repo
+    fetchfrom = repo.pushlog.lastpushid() + 1
+    lines = pullop.remote._call('pushlog', firstpush=str(fetchfrom))
+    lines = iter(lines.splitlines())
+
+    statusline = lines.next()
+    if statusline[0] == '0':
+        raise Abort('error fetching pushlog: %s' % lines[1])
+    elif statusline != '1':
+        raise Abort('error fetching pushlog: unexpected response: %s\n' %
+            statusline)
+
+    pushes = []
+    for line in lines:
+        pushid, who, when, nodes = line.split(' ', 3)
+        nodes = [bin(n) for n in nodes.split()]
+        pushes.append((int(pushid), who, int(when), nodes))
+
+    repo.pushlog.recordpushes(pushes)
+    repo.ui.status('added %d pushes\n' % len(pushes))
+
+    return orig(pullop)
 
 class pushlog(object):
     '''An interface to pushlog data.'''
@@ -76,6 +157,72 @@ class pushlog(object):
 
             c.commit()
 
+    def recordpushes(self, pushes):
+        """Record multiple pushes.
+
+        This is effectively a version of ``recordpush()`` that accepts multiple
+        pushes.
+
+        It accepts in iterable of tuples:
+
+          (pushid, user, time, nodes)
+
+        Where ``nodes`` is an iterable of changeset identifiers (both bin and
+        hex forms are accepted).
+        """
+        with self.conn() as c:
+            for pushid, user, when, nodes in pushes:
+                c.execute('INSERT INTO pushlog (id, user, date) VALUES (?, ?, ?)',
+                    (pushid, user, when))
+                for n in nodes:
+                    ctx = self.repo[n]
+                    rev = ctx.rev()
+                    node = ctx.hex()
+
+                    c.execute('INSERT INTO changesets (pushid, rev, node) '
+                        'VALUES (?, ?, ?)', (pushid, rev, node))
+
+            c.commit()
+
+    def lastpushid(self):
+        """Obtain the integer pushid of the last known push."""
+        with self.conn() as c:
+            res = c.execute('SELECT id from pushlog ORDER BY id DESC').fetchone()
+            if not res:
+                return 0
+            return res[0]
+
+    def pushes(self):
+        """Return information about pushes to this repository.
+
+        This is a generator of tuples describing each push. Each tuple has the
+        form:
+
+            (pushid, who, when, [nodes])
+
+        Nodes are returned in their 40 byte hex form.
+        """
+        with self.conn() as c:
+            res = c.execute('SELECT id, user, date, rev, node from pushlog '
+                    'LEFT JOIN changesets ON id=pushid '
+                    'ORDER BY id, rev ASC')
+
+            lastid = None
+            current = None
+            for pushid, who, when, rev, node in res:
+                who = who.encode('utf-8')
+                node = node.encode('ascii')
+                if pushid != lastid:
+                    if current:
+                        yield current
+                    lastid = pushid
+                    current = (pushid, who, when, [node])
+                else:
+                    current[3].append(node)
+
+            if current:
+                yield current
+
 def pretxnchangegrouphook(ui, repo, node=None, source=None, **kwargs):
     # This hook is executed whenever changesets are introduced. We ignore
     # new changesets unless they come from a push. ``source`` can be
@@ -101,6 +248,11 @@ def pretxnchangegrouphook(ui, repo, node=None, source=None, **kwargs):
         ui.write('Error inserting into pushlog. Please retry your push.\n')
 
     return 1
+
+def extsetup(ui):
+    extensions.wrapfunction(wireproto, '_capabilities', capabilities)
+    extensions.wrapfunction(exchange.pulloperation, 'closetransaction',
+        exchangepullpushlog)
 
 def reposetup(ui, repo):
     if not repo.local():
---
+++ hgext/pushlog/tests/test-pull.t
@@ -0,0 +1,135 @@
+  $ . $TESTDIR/hghooks/tests/common.sh
+
+  $ cat >> $HGRCPATH << EOF
+  > [ui]
+  > ssh = python "$TESTDIR/pylib/mercurial-support/dummyssh"
+  > EOF
+
+  $ export USER=hguser
+  $ hg init server
+  $ cd server
+  $ cat >> .hg/hgrc << EOF
+  > [extensions]
+  > pushlog = $TESTDIR/hgext/pushlog
+  > EOF
+  $ hg serve -d -p $HGPORT --pid-file server.pid -E error.log -A access.log
+  $ cat server.pid >> $DAEMON_PIDS
+  $ cd ..
+
+  $ hg init client
+  $ cd client
+  $ touch foo
+  $ hg commit -A -m 'initial'
+  adding foo
+  $ hg push ../server
+  pushing to ../server
+  searching for changes
+  adding changesets
+  adding manifests
+  adding file changes
+  added 1 changesets with 1 changes to 1 files
+  Trying to insert into pushlog.
+  Please do not interrupt...
+  Inserted into the pushlog db successfully.
+
+Pulling from a remote that has pushlog will fetch pushlog data
+
+  $ cat >> .hg/hgrc << EOF
+  > [extensions]
+  > pushlog = $TESTDIR/hgext/pushlog
+  > EOF
+  $ hg pull http://localhost:$HGPORT
+  pulling from http://localhost:$HGPORT/
+  searching for changes
+  no changes found
+  added 1 pushes
+
+  $ dumppushlog client
+  ID: 1; user: hguser; Date: \d+; Rev: 0; Node: 96ee1d7354c4ad7372047672c36a1f561e3a6a4c (re)
+
+  $ cd ..
+  $ hg -q clone http://localhost:$HGPORT client2
+  $ cd client2
+  $ echo foo2 > foo
+  $ hg commit -m 'second'
+  $ hg -q push ../server
+  Trying to insert into pushlog.
+  Please do not interrupt...
+  Inserted into the pushlog db successfully.
+  $ echo foo3 > foo
+  $ hg commit -m 'third'
+  $ echo foo4 > foo
+  $ hg commit -m 'fourth'
+  $ hg -q push ../server
+  Trying to insert into pushlog.
+  Please do not interrupt...
+  Inserted into the pushlog db successfully.
+  $ cd ..
+
+  $ cd client
+  $ hg pull http://localhost:$HGPORT
+  pulling from http://localhost:$HGPORT/
+  searching for changes
+  adding changesets
+  adding manifests
+  adding file changes
+  added 3 changesets with 3 changes to 1 files
+  (not updating pushlog since changesets come from pull)
+  added 2 pushes
+  (run 'hg update' to get a working copy)
+  $ dumppushlog client
+  ID: 1; user: hguser; Date: \d+; Rev: 0; Node: 96ee1d7354c4ad7372047672c36a1f561e3a6a4c (re)
+  ID: 2; user: hguser; Date: \d+; Rev: 1; Node: d0fddd3a3fb51076c33010ecf66692621f989a2c (re)
+  ID: 3; user: hguser; Date: \d+; Rev: 2; Node: 53532d3f0b038c6e7bf435c45f28c1be1ab97049 (re)
+  ID: 3; user: hguser; Date: \d+; Rev: 3; Node: 77a9042f9fafe759713d8e76d027e55fee784512 (re)
+
+  $ cd ..
+
+Cloning from scratch will retrieve multiple pushlog entries
+
+  $ hg -q --config extensions.pushlog=$TESTDIR/hgext/pushlog clone http://localhost:$HGPORT clone-multiple
+  $ dumppushlog clone-multiple
+  ID: 1; user: hguser; Date: \d+; Rev: 0; Node: 96ee1d7354c4ad7372047672c36a1f561e3a6a4c (re)
+  ID: 2; user: hguser; Date: \d+; Rev: 1; Node: d0fddd3a3fb51076c33010ecf66692621f989a2c (re)
+  ID: 3; user: hguser; Date: \d+; Rev: 2; Node: 53532d3f0b038c6e7bf435c45f28c1be1ab97049 (re)
+  ID: 3; user: hguser; Date: \d+; Rev: 3; Node: 77a9042f9fafe759713d8e76d027e55fee784512 (re)
+
+Cloning over SSH will fetch pushlog data
+
+  $ hg -q --config extensions.pushlog=$TESTDIR/hgext/pushlog clone ssh://user@dummy/$TESTTMP/server clone-ssh
+  $ dumppushlog clone-ssh
+  ID: 1; user: hguser; Date: \d+; Rev: 0; Node: 96ee1d7354c4ad7372047672c36a1f561e3a6a4c (re)
+  ID: 2; user: hguser; Date: \d+; Rev: 1; Node: d0fddd3a3fb51076c33010ecf66692621f989a2c (re)
+  ID: 3; user: hguser; Date: \d+; Rev: 2; Node: 53532d3f0b038c6e7bf435c45f28c1be1ab97049 (re)
+  ID: 3; user: hguser; Date: \d+; Rev: 3; Node: 77a9042f9fafe759713d8e76d027e55fee784512 (re)
+
+Incremental pushlog fetch works over SSH
+
+  $ cd client2
+  $ echo ssh-incremental > foo
+  $ hg commit -m 'ssh-incremental'
+  $ hg -q push ../server
+  Trying to insert into pushlog.
+  Please do not interrupt...
+  Inserted into the pushlog db successfully.
+
+  $ cd ../clone-ssh
+  $ hg --config extensions.pushlog=$TESTDIR/hgext/pushlog pull
+  pulling from ssh://user@dummy/$TESTTMP/server
+  searching for changes
+  adding changesets
+  adding manifests
+  adding file changes
+  added 1 changesets with 1 changes to 1 files
+  (not updating pushlog since changesets come from pull)
+  added 1 pushes
+  (run 'hg update' to get a working copy)
+
+  $ dumppushlog clone-ssh
+  ID: 1; user: hguser; Date: \d+; Rev: 0; Node: 96ee1d7354c4ad7372047672c36a1f561e3a6a4c (re)
+  ID: 2; user: hguser; Date: \d+; Rev: 1; Node: d0fddd3a3fb51076c33010ecf66692621f989a2c (re)
+  ID: 3; user: hguser; Date: \d+; Rev: 2; Node: 53532d3f0b038c6e7bf435c45f28c1be1ab97049 (re)
+  ID: 3; user: hguser; Date: \d+; Rev: 3; Node: 77a9042f9fafe759713d8e76d027e55fee784512 (re)
+  ID: 4; user: hguser; Date: \d+; Rev: 4; Node: f77647c7d4e3e4728ad0de09ffd09f8cf5a160a1 (re)
+
+  $ cd ..
