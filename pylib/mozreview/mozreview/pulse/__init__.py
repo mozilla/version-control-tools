55e83e290b04c1495a2aca9b4afe1ae9b4538f1e
Steven MacLeod <smacleod@mozilla.com>
2015-01-20 11:45:01 -0500

    mozreview: Publish review_request_published to Pulse. r=gps r=mcote
    A Pulse message is now published when a review request is published.
    Currently it is routed as mozreview.review_request.published and only
    contains the review request's id in data['id'].
    
    --HG--
    extra : rebase_source : 5954e586711fec2dc6aaffc239cbffa4641e529a
    extra : histedit_source : 3ee8c33721eb100324406f885a835bf30fe4e6cf

--- pylib/mozreview/mozreview/pulse/__init__.py
+++ pylib/mozreview/mozreview/pulse/__init__.py
@@ -1,6 +1,9 @@
 from reviewboard.extensions.hooks import SignalHook
 from reviewboard.reviews.signals import review_request_published
 
+from mozillapulse import publishers
+from mozillapulse.messages import base
+
 from mozreview.decorators import if_ext_enabled
 
 
@@ -12,3 +15,26 @@ def initialize_pulse_handlers(extension):
 @if_ext_enabled
 def handle_review_request_published(extension=None, **kwargs):
     review_request = kwargs.get('review_request')
+
+    # TODO: Expand this message to make it useful
+    msg = base.GenericMessage()
+    msg.routing_parts.append('mozreview.review_request.published')
+    msg.data['id'] = review_request.id
+
+    publish_message(extension, msg)
+
+def publish_message(extension, msg):
+    config = get_pulse_config(extension)
+    pulse = publishers.MozReviewPublisher(**config)
+
+    try:
+        pulse.publish(msg)
+    finally:
+        pulse.disconnect()
+
+def get_pulse_config(extension):
+    return {
+        'host': extension.settings['pulse_host'] or None,
+        'user': extension.settings['pulse_user'] or None,
+        'password': extension.settings['pulse_password'] or None,
+    }
