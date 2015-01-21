c9c799e69e768a55efb621c523683658b80df077
Steven MacLeod <smacleod@mozilla.com>
2015-01-21 11:26:36 -0500

    mozreview: Send a useful message when new commits are published (Bug 1124259). r=gps
    We replace the old 'mozreview.review_request.published' message with a
    more focused 'mozreview.commits.published' which only fires when new
    commit information is published for a MozReview request.
    
    In the future we could send generic messages for any publish in addition
    to this focused message.
    
    --HG--
    extra : rebase_source : 2690980a57499349a58305663435e2e554e7369f
    extra : amend_source : b093c25c8e05cd2262b7b36fb26d6cf1feed7f1b

--- pylib/mozreview/mozreview/pulse/__init__.py
+++ pylib/mozreview/mozreview/pulse/__init__.py
@@ -1,3 +1,5 @@
+import json
+
 from reviewboard.extensions.hooks import SignalHook
 from reviewboard.reviews.signals import review_request_published
 
@@ -10,11 +12,19 @@ from mozreview.utils import is_parent, is_pushed
 
 def initialize_pulse_handlers(extension):
     SignalHook(extension, review_request_published,
-               handle_review_request_published)
+               handle_commits_published)
 
 
 @if_ext_enabled
-def handle_review_request_published(extension=None, **kwargs):
+def handle_commits_published(extension=None, **kwargs):
+    """Handle sending 'mozreview.commits.published'.
+
+    This message is only sent when the parent review request, in a set of
+    pushed review requests, is published with new commit information.
+
+    This is a useful message for consumers who care about new or modified
+    commits being published for review.
+    """
     review_request = kwargs.get('review_request')
 
     if (review_request is None or
@@ -22,10 +32,29 @@ def handle_review_request_published(extension=None, **kwargs):
         not is_parent(review_request)):
         return
 
-    # TODO: Expand this message to make it useful
+    # Check the change description and only continue if it contains a change
+    # to the commit information. Currently change descriptions won't include
+    # information about our extra data field, so we'll look for a change to
+    # the diff which is mandatory if the commits changed. TODO: Properly use
+    # the commit information once we start populating the change description
+    # with it.
+    cd = kwargs.get('changedesc')
+    if (cd is None or
+        'diff' not in cd.fields_changed or
+        'added' not in cd.fields_changed['diff']):
+        return
+
+    # TODO: Find a better place to retrieve the repository url since we might
+    # want it to be different from path here. This will require a new convention
+    # for where to store it, mirror_path might work.
+    repo_url = review_request.repository.path
+    commits = json.loads(review_request.extra_data.get('p2rb.commits', '[]'))
+
     msg = base.GenericMessage()
-    msg.routing_parts.append('mozreview.review_request.published')
-    msg.data['id'] = review_request.id
+    msg.routing_parts.append('mozreview.commits.published')
+    msg.data['parent_review_request_id'] = review_request.id
+    msg.data['commits'] = commits
+    msg.data['repository_url'] = repo_url
 
     publish_message(extension, msg)
 
