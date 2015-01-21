8bd45cfb614c5e753f7723cc2229008cf5ce448a
Gregory Szorc <gps@mozilla.com>
2015-01-21 10:32:58 -0500

    testing: add CLI for interacting with Pulse
    We are starting to integrate with Pulse in MozReview. Add a mach-based
    CLI for interacting with a Pulse instance.
    
    Currently, it just creates an exchange. Functionality will be expanded
    in the future.
    
    --HG--
    extra : amend_source : 75a46d4d7f983a04fdd3836e836e31a1f78d419d
    extra : histedit_source : a03f2bd02a2f4f56c5fb666b517c50b46c2bb481

---
+++ testing/vcttesting/pulse_mach_commands.py
@@ -0,0 +1,40 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import os
+
+from mach.decorators import (
+    CommandArgument,
+    CommandProvider,
+    Command,
+)
+
+@CommandProvider
+class PulseCommands(object):
+    def _get_consumer(self, class_name):
+        import mozillapulse.consumers as consumers
+
+        cls = getattr(consumers, class_name)
+        pulse_host = None
+        pulse_port = None
+
+        if 'PULSE_HOST' in os.environ:
+            pulse_host = os.environ['PULSE_HOST']
+        if 'PULSE_PORT' in os.environ:
+            pulse_port = int(os.environ['PULSE_PORT'])
+
+        if not pulse_host:
+            raise Exception('Can not find Pulse host. Try setting PULSE_HOST')
+        if not pulse_port:
+            raise Exception('Can not find Pulse port. Try setting PULSE_PORT')
+
+        return cls(user='guest', password='guest', host=pulse_host,
+                port=pulse_port)
+
+    @Command('create-exchange', category='pulse',
+        description='Create an exchange')
+    @CommandArgument('classname',
+        help='Pulse class name of exchange to initialize')
+    def create_exchange(self, classname):
+        self._get_consumer(classname)
