e506fa5f789674be73f5836dbeb1bd156c9f1918
Ed Morley <emorley@mozilla.com>
2015-03-02 21:34:45 +0000

    hghooks: remove unnecessary treeclosure_comm_central.py try-except (bug 1138089)

--- hghooks/mozhghooks/treeclosure_comm_central.py
+++ hghooks/mozhghooks/treeclosure_comm_central.py
@@ -43,36 +43,29 @@ def hook(ui, repo, node, source=None, **kwargs):
     if source == 'strip':
         return 0
 
-    try:
-        # First find out which trees are affected
-        apps = { 'thunderbird' : False,
-                 'seamonkey' : False }
+    # First find out which trees are affected
+    apps = {'thunderbird': False,
+            'seamonkey': False}
 
-        # all changesets from node to 'tip' inclusive are part of this push
-        rev = repo.changectx(node).rev()
-        tip = repo.changectx('tip').rev()
-        for i in range(rev, tip+1):
-            ctx = repo.changectx(i)
-            for changedFile in ctx.files():
-                if isOwned(changedFile, seamonkeyOwns):
-                    apps['seamonkey'] = True
-                elif isOwned(changedFile, instantbirdOwns):
-                    pass  # ignore Instantbird for tree closure reasons
-                else:
-                    apps['thunderbird'] = True
+    # all changesets from node to 'tip' inclusive are part of this push
+    rev = repo.changectx(node).rev()
+    tip = repo.changectx('tip').rev()
+    for i in range(rev, tip+1):
+        ctx = repo.changectx(i)
+        for changedFile in ctx.files():
+            if isOwned(changedFile, seamonkeyOwns):
+                apps['seamonkey'] = True
+            elif isOwned(changedFile, instantbirdOwns):
+                pass  # ignore Instantbird for tree closure reasons
+            else:
+                apps['thunderbird'] = True
 
-        repoName = os.path.basename(repo.root)
+    repoName = os.path.basename(repo.root)
 
-        for app in apps:
-            if apps[app]:
-                treestatus_name = "%s-%s" % (repoName, app)
-                if not isPushAllowed(repo, treestatus_name):
-                    return 1
+    for app in apps:
+        if apps[app]:
+            treestatus_name = "%s-%s" % (repoName, app)
+            if not isPushAllowed(repo, treestatus_name):
+                return 1
 
-    except IOError, (err):
-        #TODO: Below obsolete?
-        # fail open, I guess. no sense making hg unavailable
-        # if the wiki is down
-        print "IOError: %s" % err
-        pass
     return 0
