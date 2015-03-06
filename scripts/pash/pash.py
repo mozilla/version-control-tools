899b2fa3b8f9ab7852c60fbc3baa2f495d26cd36
Gregory Szorc <gps@mozilla.com>
2015-03-05 17:47:16 -0800

    pash: move non-root login code path to isolated function (bug 1140214); r=bkero
    We want the root login code path to be clean so that things like import
    errors don't result in root login being denied. This commit moves the
    non-root login code path to a separate function. We also move imports
    into this function so they aren't evaluated until a non-root login is
    performed.
    
    With this commit, the root login code path is now only requires a
    pash.py file with proper Python syntax and a working Python installation
    (since all we import are modules from the core Python distribution).
    
    --HG--
    extra : rebase_source : 737cf174a470e27e4c6c6d56ee3775a6a3518ff4
    extra : histedit_source : ca888c15ab74d1b075c657bdcac5dc77285f1e67

--- scripts/pash/pash.py
+++ scripts/pash/pash.py
@@ -1,13 +1,7 @@
 #! /usr/bin/python -u
 
-from datetime import datetime
-import logging
 import os
 import pwd
-import sys
-
-import hg_helper
-import ldap_helper
 
 
 def QuoteForPOSIX(string):
@@ -31,6 +25,49 @@ def QuoteForPOSIX(string):
     return "\\'".join("'" + p + "'" for p in string.split("'"))
 
 
+def process_non_root_login(user):
+    # Delay import so these don't interfere with root login code path.
+    from datetime import datetime
+    import logging
+    import sys
+    import hg_helper
+    import ldap_helper
+    server_port = os.environ.get('SSH_CONNECTION').split()[-1]
+
+    user_status = hg_helper.is_valid_user(user)
+    if user_status == 2:
+        sys.stderr.write('Your mercurial account has been disabled due \
+                          to inactivity.\nPlease file a bug at \
+                          https://bugzilla.mozilla.org (or \
+                          http://tinyurl.com/2aveg9k) to re-activate \
+                          your account.\n')
+        sys.exit(0)
+
+    elif user_status != 1:
+        sys.stderr.write('You do not have a valid mercurial account!\n')
+        sys.exit(0)
+
+    # Run ldap access date toucher, silently fail and log if we're unable to write
+    try:
+       ldap_helper.update_ldap_attribute(user, 'hgAccessDate',
+                                         datetime.utcnow().strftime("%Y%m%d%H%M%S.%fZ"),
+                                         'ldap://ldap.db.scl3.mozilla.com',
+                                         'ldap://ldapsync1.db.scl3.mozilla.com')
+    except Exception:
+       logging.basicConfig(filename='/var/log/pash.log', level=logging.DEBUG)
+       logging.exception('Failed to update LDAP attributes for %s' % user)
+
+    # hg.mozilla.org handler
+    if server_port == "22":
+        hg_helper.serve('hg.mozilla.org')
+
+    # hg.ecmascript.org handler
+    elif server_port == "222":
+        hg_helper.serve('hg.ecmascript.org')
+
+    sys.exit(0)
+
+
 if __name__ == '__main__':
     os.environ['PYTHONPATH'] = '/repo/hg/libraries/'
 
@@ -44,37 +81,4 @@ if __name__ == '__main__':
         else:
             os.execl(root_shell, root_shell)
     else:
-        server_port = os.environ.get('SSH_CONNECTION').split ()[-1]
-
-        user_status = hg_helper.is_valid_user(user)
-        if user_status == 2:
-            sys.stderr.write('Your mercurial account has been disabled due \
-                              to inactivity.\nPlease file a bug at \
-                              https://bugzilla.mozilla.org (or \
-                              http://tinyurl.com/2aveg9k) to re-activate \
-                              your account.\n')
-            sys.exit(0)
-
-        elif user_status != 1:
-            sys.stderr.write('You do not have a valid mercurial account!\n')
-            sys.exit(0)
-
-        # Run ldap access date toucher, silently fail and log if we're unable to write
-        try:
-           ldap_helper.update_ldap_attribute(user, 'hgAccessDate',
-                                             datetime.utcnow().strftime("%Y%m%d%H%M%S.%fZ"),
-                                             'ldap://ldap.db.scl3.mozilla.com',
-                                             'ldap://ldapsync1.db.scl3.mozilla.com')
-        except Exception:
-           logging.basicConfig(filename='/var/log/pash.log', level=logging.DEBUG)
-           logging.exception('Failed to update LDAP attributes for %s' % user)
-
-        # hg.mozilla.org handler
-        if server_port == "22":
-            hg_helper.serve('hg.mozilla.org')
-
-        # hg.ecmascript.org handler
-        elif server_port == "222":
-            hg_helper.serve('hg.ecmascript.org')
-
-        sys.exit (0)
+        process_non_root_login(user)
