d965a2e2b64257333f02bebce45ef5fb540a4de5
Gregory Szorc <gps@mozilla.com>
2015-03-09 22:53:26 -0700

    testing: build all Docker images when running tests

--- run-mercurial-tests.py
+++ run-mercurial-tests.py
@@ -270,7 +270,8 @@ if __name__ == '__main__':
         if build_docker:
             print('generating Docker images needed for tests')
             t_start = time.time()
-            db_image, bmoweb_image, pulse_image, autolanddb_image, autoland_image = docker.build_mozreview(verbose=verbose)
+            mr_images, hgmo_images = docker.build_all_images(verbose=verbose)
+            db_image, bmoweb_image, pulse_image, autolanddb_image, autoland_image = mr_images
             t_end = time.time()
             print('got Docker images in %.2fs' % (t_end - t_start))
             os.environ['DOCKER_BMO_DB_IMAGE'] = db_image
@@ -279,6 +280,9 @@ if __name__ == '__main__':
             os.environ['DOCKER_AUTOLANDDB_IMAGE'] = autolanddb_image
             os.environ['DOCKER_AUTOLAND_IMAGE'] = autoland_image
             #os.environ['DOCKER_RB_WEB_IMAGE'] = rbweb_image
+            os.environ['DOCKER_HGMASTER_IMAGE'] = hgmo_images['hgmaster']
+            os.environ['DOCKER_HGWEB_IMAGE'] = hgmo_images['hgweb']
+            os.environ['DOCKER_LDAP_IMAGE'] = hgmo_images['ldap']
 
         for c in docker.client.containers(all=True):
             preserve_containers.add(c['Id'])
