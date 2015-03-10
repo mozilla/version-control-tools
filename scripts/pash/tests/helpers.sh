c34c7f5bdfa56fb546f3ece9a26d2c185837baed
Gregory Szorc <gps@mozilla.com>
2015-03-10 13:09:20 -0700

    pash: create test user as a member of scm_level_1

--- scripts/pash/tests/helpers.sh
+++ scripts/pash/tests/helpers.sh
@@ -21,7 +21,7 @@ EOF
 alias testuserssh='ssh -F ssh_config -i testuser -l user@example.com -p $HGPORT'
 
 standarduser() {
-  hgmo create-ldap-user user@example.com testuser 1500 'Test User' --key-file testuser
+  hgmo create-ldap-user user@example.com testuser 1500 'Test User' --key-file testuser --scm-level 1
   cat >> $HGRCPATH << EOF
 [ui]
 ssh = ssh -F `pwd`/ssh_config -i `pwd`/testuser -l user@example.com
