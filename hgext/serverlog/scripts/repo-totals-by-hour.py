62867275b7b76f301371404dc12fc56f5fd9f83e
Gregory Szorc <gps@mozilla.com>
2015-03-19 18:00:08 -0700

    serverlog: add a script to extract hourly per-repo totals
    --HG--
    extra : amend_source : d561609076a5f8deea294b9e30cbd2bc7b95a5e4

---
+++ hgext/serverlog/scripts/repo-totals-by-hour.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import datetime
+import sys
+
+def totals_by_hour(fh):
+    hours = {}
+
+    for line in fh:
+        parts = line.rstrip().split()
+
+        try:
+            when, repo, ip, command, size, t_wall, t_cpu = parts
+            when = datetime.datetime.strptime(when, '%Y-%m-%dT%H:%M:%S')
+        except (TypeError, ValueError):
+            continue
+
+        size = int(size)
+        t_wall = float(t_wall)
+        t_cpu = float(t_cpu)
+
+        t = when.time().replace(minute=0, second=0, microsecond=0)
+        when = when.combine(when.date(), t)
+
+        repos = hours.setdefault(when, {})
+        totals = repos.setdefault(repo, [0, 0.0, 0.0])
+        totals[0] += size
+        totals[1] += t_wall
+        totals[2] += t_cpu
+
+    for date, repos in sorted(hours.items()):
+        for repo, totals in sorted(repos.items()):
+            print('%s\t%s\t%d\t%d\t%d' % (
+                date.isoformat(), repo, totals[0], totals[1], totals[2]))
+
+if __name__ == '__main__':
+    totals_by_hour(sys.stdin)
