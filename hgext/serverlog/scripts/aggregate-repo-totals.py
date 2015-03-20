014ef5b56d504d2a1fc4c947dcab253c9a13a76a
Gregory Szorc <gps@mozilla.com>
2015-03-20 15:12:14 -0700

    serverlog: allow to filter repo totals by requests count
    --HG--
    extra : rebase_source : 0be7f7f05551c11a2eb46c0299f251c326e41562

--- hgext/serverlog/scripts/aggregate-repo-totals.py
+++ hgext/serverlog/scripts/aggregate-repo-totals.py
@@ -6,7 +6,7 @@
 from optparse import OptionParser
 import sys
 
-def aggregate(fh, min_size=0):
+def aggregate(fh, min_size=0, min_requests=0):
     entries = {}
 
     for line in fh:
@@ -25,6 +25,8 @@ def aggregate(fh, min_size=0):
         totals[3] += t_cpu
 
     for (date, repo), totals in sorted(entries.items()):
+        if min_requests and totals[0] < min_requests:
+            continue
         if min_size and totals[1] < min_size:
             continue
 
@@ -35,7 +37,10 @@ if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option('--min-size', type=int, default=0,
             help='Filter entries with transfer size less than this amount')
+    parser.add_option('--min-requests', type=int, default=0,
+            help='Filter entries with at least this many requests per interval')
 
     options, args = parser.parse_args()
 
-    aggregate(sys.stdin, min_size=options.min_size)
+    aggregate(sys.stdin, min_size=options.min_size,
+              min_requests=options.min_requests)
