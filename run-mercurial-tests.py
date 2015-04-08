67246d4dbde8315d3fda73b51e7b73145adb5ea2
Gregory Szorc <gps@mozilla.com>
2015-04-08 11:16:34 -0700

    testing: relicense run-mercurial-tests.py to MPL
    It was GPL because it was calling into GPL'd Mercurial functions. We no
    longer call into Mercurial code, so there is no requirement for the GPL.
    
    I own blame for every line in this file. As the sole author, I believe
    it is within my right to relicense this code as I see fit. And I choose
    MPL, as that is the default for all code in this repo.
    
    --HG--
    extra : rebase_source : 7fafc2a208cd5ddf11e84ae36be1b7be093b7d8c

--- run-mercurial-tests.py
+++ run-mercurial-tests.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-
-# This software may be used and distributed according to the terms of the
-# GNU General Public License version 2 or any later version.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 # This file is used to run all Mercurial-related tests in this repository.
 
