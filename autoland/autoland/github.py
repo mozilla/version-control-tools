72902b9434810a30187605bb3660085cd485e525
Dan Minor <dminor@mozilla.com>
2015-05-22 11:55:25 -0400

    autoland: add mock github3 implementation for testing
    --HG--
    extra : rebase_source : c013b12ebcb65d510e657b7a27a376f0ce7bcb05
    extra : amend_source : a8f8640ffdf05ee4a4615f47ddf1536296a13d6b

--- autoland/autoland/github.py
+++ autoland/autoland/github.py
@@ -2,9 +2,48 @@ import github3
 import json
 import os
 
+class MockGithub3(object):
+    """ This class mocks out the github3 stuff we use to allow us to test
+        without access to github. """
+
+    def pull_request(self, user, repo, pullrequest):
+        class PullRequest(object):
+            def commits(self):
+                class Commit(object):
+                    sha = '05830c796e2b0e9049c9e9cd463d987f4aedf4a35'
+                    def patch(self):
+                        return """# HG changeset patch
+# User Cthulhu <cthulhu@mozilla.com>
+# Date 1428426710 14400
+#      Tue Apr 07 13:11:50 2015 -0400
+# Node ID 5830c796e2b0e9049c9e9cd463d987f4aedf4a35
+# Parent  0000000000000000000000000000000000000000
+bug 1 - did stuff
+
+diff --git a/hello b/hello
+new file mode 100644
+--- /dev/null
++++ b/hello
+@@ -0,0 +1,1 @@
++hello, world!"""
+                return [Commit()]
+        return PullRequest()
+
+    def issue(self, user, repo, issue):
+        class Issue(object):
+           def create_comment(self, comment):
+                return True
+        return Issue()
+
+
 def connect():
     with open('config.json') as f:
-        credentials = json.load(f)['github']
+        config = json.load(f)
+        credentials = config['github']
+        testing = config.get('testing', False)
+
+    if testing:
+        return MockGithub3()
 
     return github3.login(credentials['user'], password=credentials['passwd'])
 
