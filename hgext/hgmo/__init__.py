d1f282ecf7d39d70f31550e5e4aebf5be4356fc0
Gregory Szorc <gps@mozilla.com>
2015-06-03 22:26:09 -0700

    hgmo: expose and hyperlink revset for reviewers (bug 1171321); r=smacleod
    What's better than listing reviewers in changeset output? Linking
    to other work that reviewer did. This commit implements that.
    
    We implement a "reviewer" revset that looks up changesets that were
    reviewed by someone. We mark it as a safe symbol so it can be exposed
    to hgweb's search feature. We finally hyperlink reviewers in
    changeset output so you can easily find other changesets that they
    reviewed.
    
    Sadly, while developing this feature, I realized that the upstream
    work I did to expose extra metadata to the templater did not
    include search results. So, we're unable to display pushlog
    and other metadata in the search results page yet. Boo.
    
    --HG--
    extra : commitid : ExqFd6sW3CI
    extra : rebase_source : 38dd82779293b04eb846c83bcbe8c6c3b74a83ab
    extra : histedit_source : 1a84d9e23f0652ce7b93e9572314ace3e95cee15

--- hgext/hgmo/__init__.py
+++ hgext/hgmo/__init__.py
@@ -5,9 +5,12 @@
 
 import os
 
+import mercurial.encoding as encoding
 import mercurial.errors as errors
 import mercurial.extensions as extensions
 import mercurial.hgweb.webutil as webutil
+import mercurial.revset as revset
+
 
 OUR_DIR = os.path.dirname(__file__)
 execfile(os.path.join(OUR_DIR, '..', 'bootstrap.py'))
@@ -25,7 +28,12 @@ def addmetadata(repo, ctx, d):
             'url': 'https://bugzilla.mozilla.org/show_bug.cgi?id=%s' % bug,
         })
 
-    d['reviewers'] = list(commitparser.parse_reviewers(ctx.description()))
+    d['reviewers'] = []
+    for reviewer in commitparser.parse_reviewers(ctx.description()):
+        d['reviewers'].append({
+            'name': reviewer,
+            'revset': 'reviewer(%s)' % reviewer,
+        })
 
     # Obtain the Gecko/app version/milestone.
     #
@@ -56,6 +64,29 @@ def changelistentry(orig, web, ctx, tmpl):
     return d
 
 
+def revset_reviewer(repo, subset, x):
+    """``reviewer(REVIEWER)``
+
+    Changesets reviewed by a specific person.
+    """
+    l = revset.getargs(x, 1, 1, 'reviewer requires one argument')
+    n = encoding.lower(revset.getstring(l[0], 'reviewer requires a string'))
+
+    # Do not use a matcher here because regular expressions are not safe
+    # for remote execution and may DoS the server.
+    def hasreviewer(r):
+        for reviewer in commitparser.parse_reviewers(repo[r].description()):
+            if encoding.lower(reviewer) == n:
+                return True
+
+        return False
+
+    return subset.filter(hasreviewer)
+
+
 def extsetup(ui):
     extensions.wrapfunction(webutil, 'changesetentry', changesetentry)
     extensions.wrapfunction(webutil, 'changelistentry', changelistentry)
+
+    revset.symbols['reviewer'] = revset_reviewer
+    revset.safesymbols.add('reviewer')
--- hgext/hgmo/tests/test-reviewers.t
+++ hgext/hgmo/tests/test-reviewers.t
@@ -16,16 +16,23 @@ Single reviewer works
 
   $ http http://localhost:$HGPORT/rev/b2695d7dbd02 --body-file body > /dev/null
   $ grep '<td>reviewers' body
-  <tr><td>reviewers</td><td>gps</td></tr>
+  <tr><td>reviewers</td><td><a href="/log?rev=reviewer%28gps%29&revcount=50">gps</a></td></tr>
 
 Multiple reviewers separated by comma
 
   $ http http://localhost:$HGPORT/rev/e32726f11326 --body-file body > /dev/null
   $ grep '<td>reviewers' body
-  <tr><td>reviewers</td><td>gps, smacleod</td></tr>
+  <tr><td>reviewers</td><td><a href="/log?rev=reviewer%28gps%29&revcount=50">gps</a>, <a href="/log?rev=reviewer%28smacleod%29&revcount=50">smacleod</a></td></tr>
 
 No reviewer output if no reviewers
 
   $ http http://localhost:$HGPORT/rev/f081f6d27c72 --body-file body > /dev/null
   $ grep '<td>reviewers' body
   [1]
+
+Reviewer revset works
+
+  $ http http://localhost:$HGPORT/log?rev=reviewer%28gps%29 --body-file body > /dev/null
+  $ grep '<a href="/rev/' body
+  <a href="/rev/e32726f11326">diff</a><br/>
+  <a href="/rev/b2695d7dbd02">diff</a><br/>
