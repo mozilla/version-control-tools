e33a95d920797ee42216abad73fd59e82814976f
Steven MacLeod <smacleod@mozilla.com>
2015-06-09 23:31:03 -0400

    mozreview: Create model for storing MozReview user information (Bug 1160517). r=mcote
    Having a MozReviewUserProfile models allows MozReview to tie extra
    information to a Review Board user. Before this commit that has been
    done using a separate table for the specific information (such as the
    BugzillaUserMap).
    
    In order to support querying ldap for user permissions an ldap username
    must be stored for each Review Board user. Rather than make a specific
    table for this, introduce a generic MozReviewUserProfile and store the
    information there.
    
    --HG--
    extra : rebase_source : e22a9846419fcda48f4d63c88b9d63d74ce99377

--- pylib/mozreview/mozreview/models.py
+++ pylib/mozreview/mozreview/models.py
@@ -1,5 +1,8 @@
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
+from django.db import models
+
 from mozreview.autoland.models import (AutolandEventLogEntry,
                                        AutolandRequest)
 from mozreview.bugzilla.models import (BugzillaUserMap,
@@ -10,4 +13,33 @@ __all__ = [
     'AutolandRequest',
     'BugzillaUserMap',
     'get_or_create_bugzilla_users',
+    'MozReviewUserProfile',
 ]
+
+
+def get_profile(user):
+    """Return the MozReviewUserProfile associated with a user.
+
+    If the MozReviewUserProfile object does not exist a default
+    one will be created.
+    """
+    return MozReviewUserProfile.objects.get_or_create(user=user)[0]
+
+
+class MozReviewUserProfile(models.Model):
+    """Extra User Profile information for MozReview"""
+    user = models.OneToOneField(User, primary_key=True)
+
+    # ldap username associated with the user. Blank indicates
+    # the RB user has not yet been associated with an ldap account.
+    # A non blank value indicates the user has proven they control
+    # that ldap username and should be given the permissions
+    # associated with its groups in ldap.
+    ldap_username = models.CharField(max_length=256, blank=True)
+
+    class Meta:
+        app_label = 'mozreview'
+        permissions = (
+            ("modify_ldap_association",
+             "Can change ldap assocation for all users"),
+        )
