d148b79b4426ec9a1060867fe8dceacdb37bf4d1
Steven MacLeod <smacleod@mozilla.com>
2015-06-10 18:05:06 -0400

    mozreview: Add function to update ldap association (Bug 1160517). r=gps r=dminor
    Add `associate_ldap_username` function to pushhooks.py which will update
    a users associated ldap_username through the web API. This is a new
    function rather than a modification to post_reviews so that we could
    theoretically introduce a new wireprotocommand dedicated to associating
    your ldap account (which could be used by non hg developers, rather
    than having to push actual code). To start we'll probably just use it
    when submitting a review.
    
    --HG--
    extra : rebase_source : 2d0bb2e00a861b8a1d958b86ed3b46f0b84aeb90
    extra : histedit_source : f550f8ee211eb404be96e8c64feb87da49a74f25

--- pylib/reviewboardmods/reviewboardmods/pushhooks.py
+++ pylib/reviewboardmods/reviewboardmods/pushhooks.py
@@ -431,6 +431,73 @@ def _post_reviews(api_root, repoid, identifier, commits, hgresp):
     return squashed_rr.id, node_to_rid, review_requests
 
 
+def associate_ldap_username(url, ldap_username, privileged_username,
+                            privileged_password, username=None, password=None,
+                            userid=None, cookie=None):
+    """Associate a Review Board user with an ldap username.
+
+    Will return True if an ldap_username is successfully associated
+    with a Review Board account, False otherwise.
+
+    This function does not prove ownership over the provided
+    ldap_username, it assumes that has been done by the caller
+    (e.g. They have pushed to an ldap authenticated hg server
+    over ssh).
+
+    In order to associate an ldap username with a user two sets of
+    credentials needs to be provided: A user account with permission
+    to change ldap associations, and a user account to be changed
+    (privileged_username/privileged_password and
+    username/password/cookie/userid respectively).
+
+    The Review Board credentials of the user account to be changed
+    are required since we should never associate an ldap username
+    with a Review Board account unless the requester has proven
+    ownership of the Review Board account.
+    """
+    # TODO: Provide feedback on what went wrong when we fail to
+    # associate the username.
+
+    # TODO: Figure out a better way to make sure bots don't end up
+    # associating their ldap account with a Review Board user they
+    # are pushing for. Bug 1176008
+    if ldap_username == 'bind-autoland@mozilla.com':
+        return False
+
+    if not ((username is not None and password is not None) or
+            (userid is not None and cookie is not None)):
+        return False
+
+    try:
+        # We first verify that the provided credentials are valid and
+        # retrieve the username associated with that Review Board
+        # account.
+        with ReviewBoardClient(url, username, password, userid, cookie) as rbc:
+            root = rbc.get_root()
+            session = root.get_session()
+
+            if not session.authenticated:
+                return False
+
+            user = session.get_user()
+            username = user.username
+
+        # Now that we have proven ownership over the user, take the provided
+        # ldap_username and associate it with the account.
+        with ReviewBoardClient(url, privileged_username, privileged_password,
+                               None, None) as rbc:
+            root = rbc.get_root()
+            ext = root.get_extension(
+                extension_name='mozreview.extension.MozReviewExtension')
+            ldap = ext.get_ldap_associations().get_item(username)
+            ldap.update(ldap_username=ldap_username)
+
+    except APIError:
+        return False
+
+    return True
+
+
 class NoCacheTransport(SyncTransport):
     """API transport with disabled caching."""
     def enable_cache(self):
