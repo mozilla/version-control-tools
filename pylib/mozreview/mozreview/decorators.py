3e3cb0deb01b4f9f548b43640a29dfee76ffeb05
Steven MacLeod <smacleod@mozilla.com>
2015-06-19 14:03:43 -0400

    mozreview: Add webapi decorator to check scm groups (Bug 1160520). r=dminor r=mdoglio
    Add a new webapi_scm_groups_required to make restricting webapi
    operations to certain scm ldap groups easy.
    
    --HG--
    extra : rebase_source : 63802742db435f0818ca26c8e47bdddeedeb62a7

--- pylib/mozreview/mozreview/decorators.py
+++ pylib/mozreview/mozreview/decorators.py
@@ -1,4 +1,9 @@
 from djblets.util.decorators import simple_decorator
+from djblets.webapi.decorators import (_find_httprequest,
+                                       webapi_decorator,
+                                       webapi_login_required,
+                                       webapi_response_errors)
+from djblets.webapi.errors import PERMISSION_DENIED
 
 @simple_decorator
 def if_ext_enabled(fn):
@@ -24,3 +29,38 @@ def if_ext_enabled(fn):
         return fn(*args, **kwargs)
 
     return _wrapped
+
+
+def webapi_scm_groups_required(*groups):
+    """Checks that a user has required scm ldap groups."""
+    @webapi_decorator
+    def _dec(view_func):
+
+        @webapi_login_required
+        @webapi_response_errors(PERMISSION_DENIED)
+        def _check_groups(*args, **kwargs):
+            request = _find_httprequest(args)
+            mrp = request.mozreview_profile
+
+            if not mrp:
+                # This should never happen since webapi_login_required
+                # should mean they are authenticated and our middleware
+                # has added the profile to the request. Check it just
+                # to make sure nothing went wrong with the middleware.
+                logging.error('No MozReviewUserProfile for authenticated user')
+                return PERMISSION_DENIED
+
+            if not mrp.ldap_username:
+                return PERMISSION_DENIED.with_message(
+                    'You are not associated with an ldap account')
+
+            for group in groups:
+                if not mrp.has_scm_ldap_group(group):
+                    return PERMISSION_DENIED.with_message(
+                        'You do not have the required ldap permissions')
+
+            return view_func(*args, **kwargs)
+
+        return _check_groups
+
+    return _dec
