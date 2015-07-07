effd8cdff9878c4fb1504043fa3892cce4a34c40
Gregory Szorc <gps@mozilla.com>
2015-07-07 15:12:41 -0700

    serverlog: generate hourly daily aggregates
    Why not.
    
    --HG--
    extra : commitid : 2MC3j0w08ut

--- hgext/serverlog/scripts/generate-daily-logs.py
+++ hgext/serverlog/scripts/generate-daily-logs.py
@@ -28,10 +28,21 @@ subprocess.check_call('cat %s | %s/parse.py --date %s > %s' % (
     ' '.join(relevant_logs), HERE, yesterday.isoformat(), dest_log),
     shell=True)
 
+# TODO we should really have a single script that emits all variants, as
+# this would eliminate redundant parsing of the files.
+
 subprocess.check_call('cat %s | %s/totals-by-day.py > /var/log/hg/totals-by-day.%s' % (
     dest_log, HERE, yesterday.isoformat()),
     shell=True)
 
 subprocess.check_call('cat %s | %s/repo-totals-by-day.py > /var/log/hg/repo-totals-by-day.%s' % (
+    dest_log, HERE, yesterday.isoformat()),
+    shell=True)
+
+subprocess.check_call('cat %s | %s/totals-by-hour.py > /var/log/hg/totals-by-hour.%s' % (
+    dest_log, HERE, yesterday.isoformat()),
+    shell=True)
+
+subprocess.check_call('cat %s | %s/repo-totals-by-hour.py > /var/log/hg/repo-totals-by-hour.%s' % (
     dest_log, HERE, yesterday.isoformat()),
     shell=True)
