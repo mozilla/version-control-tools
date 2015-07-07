6201d8e1a327b24fe99c96b5e9e880e8482e2857
Gregory Szorc <gps@mozilla.com>
2015-07-07 16:11:39 -0700

    serverlog: teach repo-totals-by-day to print per-command stats
    Splitting our metrics by command type is useful to see what is driving
    the load.
    
    --HG--
    extra : commitid : 4CbvVZBm8eQ

--- hgext/serverlog/scripts/repo-totals-by-day.py
+++ hgext/serverlog/scripts/repo-totals-by-day.py
@@ -6,7 +6,7 @@
 import datetime
 import sys
 
-def totals_by_day(fh):
+def totals_by_day(fh, by_command=False):
     days = {}
 
     for line in fh:
@@ -25,17 +25,41 @@ def totals_by_day(fh):
         date = when.date()
 
         repos = days.setdefault(date, {})
-        totals = repos.setdefault(repo, [0, 0, 0.0, 0.0])
+        totals = repos.setdefault((repo, command), [0, 0, 0.0, 0.0])
+        all_totals = repos.setdefault((repo, 'all'), [0, 0, 0.0, 0.0])
         totals[0] += 1
+        all_totals[0] += 1
         totals[1] += size
+        all_totals[1] += size
         totals[2] += t_wall
+        all_totals[2] += t_wall
         totals[3] += t_cpu
+        all_totals[3] += t_cpu
 
     for date, repos in sorted(days.items()):
-        for repo, totals in sorted(repos.items()):
-            print('%s\t%s\t%d\t%d\t%d\t%d' % (date.isoformat(), repo,
-                                              totals[0], totals[1], totals[2],
-                                              totals[3]))
+        for (repo, command), totals in sorted(repos.items()):
+            if by_command:
+                print('%s\t%s\t%s\t%d\t%d\t%d\t%d' % (
+                    date.isoformat(),
+                    repo,
+                    command,
+                    totals[0],
+                    totals[1],
+                    totals[2],
+                    totals[3],
+                ))
+            else:
+                if command != 'all':
+                    continue
+
+                print('%s\t%s\t%d\t%d\t%d\t%d' % (
+                    date.isoformat(),
+                    repo,
+                    totals[0],
+                    totals[1],
+                    totals[2],
+                    totals[3],
+                ))
 
 if __name__ == '__main__':
-    totals_by_day(sys.stdin)
+    totals_by_day(sys.stdin, by_command='--by-command' in sys.argv)
