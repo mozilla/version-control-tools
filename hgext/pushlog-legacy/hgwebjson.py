a333480e356850fb57f725e10005f8600d4e775a
Gregory Szorc <gps@mozilla.com>
2015-08-10 16:59:19 -0700

    pushlog: use native templates for json-info command (bug 1171250); r=smacleod
    Continuing our march towards eliminating the custom "mozjson" template
    filter.
    
    --HG--
    extra : commitid : Jkpmwz98gXU
    extra : rebase_source : 6bffd7e233f8bde9cef611099fd7845feb8a97ce
    extra : histedit_source : f22703f9b9799f87f92154af7c1a8373998f35c8

--- hgext/pushlog-legacy/hgwebjson.py
+++ hgext/pushlog-legacy/hgwebjson.py
@@ -52,7 +52,24 @@ def info(web, req, tmpl):
     """Get JSON information about the specified nodes."""
     if 'node' not in req.form:
         return tmpl('error', error={'error': "missing parameter 'node'"})
-    nodes = dict((n, web.repo.changectx(n)) for n in req.form['node'])
-    return tmpl('info', nodes=nodes)
+
+    csets = []
+    for node in req.form['node']:
+        ctx = web.repo[node]
+        csets.append({
+            'rev': ctx.rev(),
+            'node': ctx.hex(),
+            'user': ctx.user(),
+            'date': ctx.date(),
+            'description': ctx.description(),
+            'branch': ctx.branch(),
+            'tags': ctx.tags(),
+            'parents': [p.hex() for p in ctx.parents()],
+            'children': [c.hex() for c in ctx.children()],
+            'files': ctx.files(),
+        })
+
+    return tmpl('info', csets=csets)
+
 
 addwebcommand(info, 'info')
