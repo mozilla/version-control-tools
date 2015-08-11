dcb6cfb54d0d32ff683aab2054fd365610a8fbbf
Gregory Szorc <gps@mozilla.com>
2015-08-10 19:11:03 -0700

    pushlog: remove unused JSON code (bug 1171250); r=smacleod
    Now that we are using native templates for everything, our custom
    "mozjson" filter is not used and can be deleted. Good riddance.
    
    --HG--
    extra : commitid : JgTZTDrNv1R
    extra : rebase_source : 67fabd129fa3601e21824ee9e4222bf6ab761b73
    extra : histedit_source : d4fe414429a3f2f5dad77f0422ede604c1f557a2

--- hgext/pushlog-legacy/hgwebjson.py
+++ hgext/pushlog-legacy/hgwebjson.py
@@ -1,47 +1,5 @@
-from mercurial import context, util
-from mercurial.node import hex, nullid
 from mercurial.hgweb import webcommands
-from mercurial import templatefilters
 
-import json
-
-isodate = lambda x: util.datestr(x, '%Y-%m-%d %H:%M %1%2')
-
-class HGJSONEncoder(json.JSONEncoder):
-    def __init__(self):
-        json.JSONEncoder.__init__(self, indent=1, sort_keys=True)
-
-    def __call__(self, obj):
-        return self.encode(obj)
-
-    def default(self, v):
-        if isinstance(v, context.changectx):
-            return {'rev':         v.rev(),
-                    'node':        hex(v.node()),
-                    'user':        v.user(),
-                    'date':        isodate(v.date()),
-                    'description': v.description(),
-                    'branch':      v.branch(),
-                    'tags':        v.tags(),
-                    'parents':     [hex(n.node()) for n in v.parents()
-                                    if n != nullid],
-                    'children':    [hex(n.node()) for n in v.children()
-                                    if n != nullid],
-                    'files':       v.files(),
-                    }
-
-        if isinstance(v, context.filectx):
-            return {'filerev':     v.filerev(),
-                    'filenode':    hex(v.filenode())}
-
-        if hasattr(v, '__iter__'):
-            return [o for o in v]
-
-        return json.JSONEncoder.default(self, v)
-
-templatefilters.filters['mozjson'] = HGJSONEncoder()
-
-# Add hgweb hooks
 
 def addwebcommand(f, name):
     setattr(webcommands, name, f)
