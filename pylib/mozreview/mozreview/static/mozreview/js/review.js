96eb2af583e56ed244610b9bd05d9cd32a1f3e25
Gregory Szorc <gps@mozilla.com>
2015-08-27 11:11:18 -0400

    mozreview: consolidate buttons for publishing reviews (bug 1195860); r=mdoglio
    The review request page currently has many buttons/links related to
    publishing/submitting review:
    
    1) A "Review" text link in the actions list that opens the review
       dialog.
    2) A "Ship It" text link in the actions list that submits the current
       review and marks it as ship it.
    3) A "Edit Review" button in the review banner that opens the review
       dialog.
    4) A "Publish" button in the review banner that publishes the review
       as-is, with no dialog for confirmation.
    
    The redundancy in buttons and subtle nuances in behavior are confusing.
    Many are confused what each button does. "Does it open a dialog for
    additional confirmation or not?" "Why are there multiple review
    buttons." "Reviews is a noun but it seems to perform an action."
    
    This commit attempts to reduce confusion by:
    
    1) Removing the "Publish" and "Ship It" buttons.
    2) Changing the text of "Edit Review" to "Finish Review" so people have
       a hint that clicking it will finalize the review.
    3) Changing the text of "Review" to "Finish Review" so it is a verb
       (like other text in the actions list) and so it matches the text
       in the review banner.
    
    The new UI is simpler and consistent.
    
    The simplicity does come at a price: power users no longer can click a
    single button to publish or ship it a review. Instead, they must open
    the review dialog and click "Publish Review" there. Since many new users
    were confused by the various buttons, I think this trade-off is
    acceptable. If people miss the old functionality, we can always add it
    back in somehow, preferably with better UI to indicate what exactly is
    going to happen. For now, we favor simplicity.
    
    --HG--
    extra : commitid : 1C1U1Wg3No5
    extra : amend_source : e0a056d9833e219fc2bc22967ced90fa1599823f

--- pylib/mozreview/mozreview/static/mozreview/js/review.js
+++ pylib/mozreview/mozreview/static/mozreview/js/review.js
@@ -12,4 +12,12 @@ $(document).on("mozreview_ready", function() {
   $("#mozreview-child-requests .editable").inlineEditor("enable");
 
   $('label[for="field_target_people"]').parent().parent().hide();
+
+  // Change string of "Edit Review" button in the review banner that
+  // shows up when a pending review is waiting to be published.
+  $("#review-banner-edit").val("Finish");
+
+  // Change string of "Review" button to be a verb so people better
+  // understand what clicking it does.
+  $("#review-link").text("Finish Review");
 });
