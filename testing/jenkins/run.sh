be47e36a7986945db254f32911bd6ce73ac546e0
Gregory Szorc <gps@mozilla.com>
2015-11-11 10:24:37 -0800

    jenkins: install modern Python in virtual machine
    This should fix issues with package manager complaining about a Python
    that doesn't support modern SSL.
    
    --HG--
    extra : commitid : XqYBRRzSno
    extra : amend_source : a4089a87919433bb0f1cb840293f6329b84358cd

--- testing/jenkins/run.sh
+++ testing/jenkins/run.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
-if [ ! -e version-control-tools ]; then
-  hg clone /version-control-tools version-control-tools
+# Build modern Python from source because Ubuntu 14.04 ships with an
+# older one that doesn't support modern SSL and this screws up packaging
+# foo.
+if [ ! -e ~/.pyenv ]; then
+  git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+fi
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
+if [ ! -e ~/.pyenv/versions/2.7.10 ]; then
+  pyenv install 2.7.10
+  pyenv rehash
 fi
 
-cd version-control-tools
+pyenv shell 2.7.10
+pip install --upgrade pip virtualenv
+
+# Install a modern Mercurial or we may run into issues cloning from the mounted
+# repo since it may be using settings not known by the older Mercurial
+# installed by Ubuntu.
+pip install --upgrade Mercurial
+
+if [ ! -e ~/version-control-tools ]; then
+  hg clone --pull /version-control-tools ~/version-control-tools
+fi
+
+cd ~/version-control-tools
 hg pull
 hg --config extensions.purge= purge
 hg up tip
