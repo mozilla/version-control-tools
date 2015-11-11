0569e1cb7224869ae421403d99d6bf325c738d55
byron jones <glob@mozilla.com>
2015-11-11 15:29:56 +0800

    mozautomation: add setup.py and add to reviewboard deployment (Bug 1220232). r=gps
    Promote mozautomation from a raw library to an installable package.
    Update mozreview's ansible and test scripts to deploy mozautomation.
    
    --HG--
    extra : commitid : JiReokWtRC4
    extra : rebase_source : b3d61ab7007e0f9e39a3bc2a6fc666a88cc96737
    extra : amend_source : 7e91784f570e0af66df064b337a667db99e9e7ff

---
+++ pylib/mozautomation/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='mozautomation',
+      version='0.1',
+      description='Support packages for interacting with parts of Mozilla\'s automation infrastructure',
+      author='Mozilla Developer Services',
+      packages = ['mozautomation'],
+)
