a986f8c6a0136125ffe201b5feb3b7975b7582d6
Dan Minor <dminor@mozilla.com>
2015-11-13 15:54:46 -0500

    testing: make mozautomation.commitparser importable from test_commitparser (bug 1192958) r=glob
    At the moment, this test is not directly runnable from run-tests. This adds
    commitparser to the system path to fix this.
    
    --HG--
    extra : rebase_source : 9bd7fd64450abe4070da9171f235f8e5c286464a

--- pylib/mozautomation/tests/test_commitparser.py
+++ pylib/mozautomation/tests/test_commitparser.py
@@ -1,6 +1,13 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+import os
+import unittest
+import sys
+
+HERE = os.path.split(os.path.realpath(__file__))[0]
+sys.path.append(os.path.split(HERE)[0])
+
 
 import unittest
 
