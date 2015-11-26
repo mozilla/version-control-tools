69d1ecf49f79d4b264699c8b13ec2f7933b5a0fd
Mauro Doglio <mdoglio@mozilla.com>
2015-11-26 16:18:40 +0000

    testing: improve coverage of FileDiffReviewerTest (bug 1227508); r=mcote
    --HG--
    extra : rebase_source : 2881dd9bbfbb99a9c535d5836a0e0182304fc8bd

--- pylib/mozreview/mozreview/tests/test-filediffreviewer-buttons.py
+++ pylib/mozreview/mozreview/tests/test-filediffreviewer-buttons.py
@@ -5,6 +5,8 @@
 from __future__ import absolute_import, unicode_literals
 
 import selenium.webdriver.support.expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 
@@ -21,6 +23,7 @@ class FileDiffReviewerTest(MozReviewWebDriverTest):
             self.create_users([
                 ('jsmith@example.com', 'password1', 'Jeremy Smith [:jsmith]'),
                 ('mjane@example.com', 'password2', 'Mary Jane [:mary]'),
+                ('jdoe@example.com', 'password3', 'John Doe [:jdoe]'),
             ])
 
             self.create_ldap(b'mjane@example.com',
@@ -33,19 +36,30 @@ class FileDiffReviewerTest(MozReviewWebDriverTest):
             lr.write('foo', 'first change')
             lr.run(['commit', '-m', 'Bug 1 - Test try;r=jsmith'])
             lr.run(['push'])
+            lr.write('foo', 'second change')
+            lr.run(['commit', '-m', 'second change;r=jsmith'])
+            lr.run(['push'])
         except Exception:
             MozReviewWebDriverTest.tearDownClass()
             raise
 
+    def _element_is_present(self, selector, timeout=10):
+        try:
+            WebDriverWait(self.browser, timeout).until(
+                EC.presence_of_element_located(selector))
+            return True
+        except TimeoutException:
+            return False
+
     def test_filediffreviewer_buttons(self):
         """Clicking on the review status should change its text and class"""
 
-        self.reviewboard_login('jsmith@example.com', 'password1')
+        self.reviewboard_login('jdoe@example.com', 'password3')
         self.load_rburl('r/1/diff/1/')
 
-        WebDriverWait(self.browser, 10).until(
-            EC.presence_of_element_located(
-                (By.CLASS_NAME, 'diff-file-btn')))
+        self.assertTrue(
+            self._element_is_present((By.CLASS_NAME, 'diff-file-btn'))
+        )
 
         review_button = self.browser.find_element_by_class_name(
             'diff-file-btn')
@@ -72,3 +86,45 @@ class FileDiffReviewerTest(MozReviewWebDriverTest):
         self.assertEqual(review_button.text, 'not reviewed')
         self.assertEqual(review_button.get_attribute('class'),
                          'diff-file-btn')
+
+        left_revision_selector = self.browser.find_elements_by_class_name(
+            'revision-selector-handle')[0]
+        right_revision_selector = self.browser.find_elements_by_class_name(
+            'revision-selector-handle')[1]
+
+        first_revision = self.browser.find_elements_by_class_name(
+            'revision-selector-tick')[1]
+        last_revision = self.browser.find_elements_by_class_name(
+            'revision-selector-tick')[2]
+
+        action_chains = ActionChains(self.browser)
+        action_chains.drag_and_drop(right_revision_selector,
+                                    first_revision).perform()
+
+        WebDriverWait(self.browser, 10).until(
+            EC.text_to_be_present_in_element(
+                (By.CSS_SELECTOR, '#diff_revision_label h1'),
+                'Diff Revision 1'
+            )
+        )
+
+        self.assertTrue(
+            self._element_is_present((By.CLASS_NAME, 'diff-file-btn'))
+        )
+
+        action_chains.drag_and_drop(right_revision_selector,
+                                    last_revision).perform()
+
+        action_chains.drag_and_drop(left_revision_selector,
+                                    first_revision).perform()
+
+        WebDriverWait(self.browser, 10).until(
+            EC.text_to_be_present_in_element(
+                (By.CSS_SELECTOR, '#diff_revision_label h1'),
+                'Changes between revision 1 and 2'
+            )
+        )
+
+        self.assertFalse(
+            self._element_is_present((By.CLASS_NAME, 'diff-file-btn'))
+        )
