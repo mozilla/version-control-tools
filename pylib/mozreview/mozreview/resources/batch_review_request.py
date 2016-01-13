926335e50ba4e6a80a45fe1c16c9dcf8261c8f33
Gregory Szorc <gps@mozilla.com>
2016-01-13 12:31:01 -0800

    mozreview: create child review requests from batch API (bug 1229468); r=smacleod, dminor
    The final step for moving the creation of review series into a unified
    web API is moving the management of child review requests to the new
    API, which this commit does.
    
    This commit is rather large. Unfortunately, there is no easy way to
    incrementally move this code since managing the state of child review
    requests is so complicated and intertwined.
    
    As part of migrating the code, the overall structure changed
    substantially. This is because we're able to do things differently from
    the context of the web api that we weren't from outside. For example, we
    can query the user database directly instead of going through the search
    interface.
    
    The best way to review this change is probably to open a copy of
    pushhooks.py in a separate window and read it alongside the new code
    in batch_review_request.py.
    
    Fortunately, our test coverage of review state is pretty robust. And, as
    you can see, there are no test changes as a result of this refactor!
    Presumably the new behavior is identical to the old behavior!
    
    There is still room to optimize this code. For example, the old code
    maintained a cache of reviewer lookups. This was dropped as part of the
    refactor. It can and should be added in later.
    
    The practical effect of this change is that review submission and
    publishing now requires far fewer HTTP requests to the Review Board Web
    API.
    
    On a local test cluster, submitting and publishing a review series of 20
    commits with a single reviewer required the following:
    
    Before: 12.0s  134 BMO requests   158 RB requests
    After:  10.0s  154 BMO requests    54 RB requests
    Delta:  -2.0s  +20 HTTP requests -104 HTTP requests
    
    (The HTTP request count includes requests during cluster startup, so it
    is artificially high to start.)
    
    The regression in BMO HTTP requests is almost certainly due to redundant
    user lookups. This will be corrected later.
    
    --HG--
    extra : commitid : AuXFcjtoYa5
    extra : rebase_source : 266c9e230842cb0a3204ce29bd49fa527ea30234
    extra : amend_source : 3574ad29a955a77c1bb04ccf003d03c844cd1aeb

--- pylib/mozreview/mozreview/resources/batch_review_request.py
+++ pylib/mozreview/mozreview/resources/batch_review_request.py
@@ -3,7 +3,9 @@ from __future__ import absolute_import, unicode_literals
 import json
 import logging
 
+from django.contrib.auth.models import User
 from django.db import transaction
+from django.db.models import Q
 from djblets.webapi.decorators import (
     webapi_login_required,
     webapi_request_fields,
@@ -13,6 +15,9 @@ from djblets.webapi.errors import (
     INVALID_FORM_DATA,
     NOT_LOGGED_IN,
     PERMISSION_DENIED,
+)
+from reviewboard.accounts.backends import (
+    get_enabled_auth_backends,
 )
 from reviewboard.diffviewer.models import (
     DiffSet,
@@ -27,10 +32,16 @@ from reviewboard.scmtools.models import (
 from reviewboard.webapi.decorators import (
     webapi_check_local_site,
 )
+from reviewboard.webapi.encoder import (
+    status_to_string,
+)
 from reviewboard.webapi.resources import (
     WebAPIResource,
 )
 
+from mozreview.review_helpers import (
+    gen_latest_reviews,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -47,6 +58,109 @@ class BatchReviewRequestResource(WebAPIResource):
     review requests being submitted. In addition, changes are only atomic
     within each HTTP request. This API exists to make submitting a review
     series a single HTTP request while also being atomic.
+
+    Each commit will become its own review request.
+    Additionally, a review request with a diff encompassing all the commits
+    will be created; This "squashed" review request will represent the push
+    for the provided ``identifier``.
+
+    The ``identifier`` is a unique string which represents a series of pushed
+    commit sets. This identifier is used to update review requests with a new
+    set of diffs from a new push. Generally this identifier will represent
+    some unit of work, such as a bug.
+
+    The ``commits`` argument takes the following form::
+
+        {
+            'squashed': {
+                'diff': <squashed-diff-string>,
+                'base_commit_id': <commit-id-to-apply-diff-to> (optional),
+                'first_public_ancestor': <commit of first public ancestor> (optional),
+            },
+            'individual': [
+                {
+                    'id': <commit-id>,
+                    'precursors': [<previous changeset>],
+                    'message': <commit-message>,
+                    'diff': <diff>,
+                    'bug': <bug-id>,
+                    'parent_diff': <diff-from-base-to-commit> (optional),
+                    'base_commit_id': <commit-id-to-apply-diffs-to> (optional),
+                    'first_public_ancestor': <commit of first public ancestor> (optional),
+                    'reviewers': [<user1>, <user2>, ...] (optional),
+                    'requal_reviewers': [<user1>, <user2>, ...] (optional),
+                },
+                {
+                    ...
+                },
+                ...
+            ]
+        }
+
+    When representing the commits on Review Board, we store meta data in the
+    extra_data dictionaries. We use a number of fields to keep track of review
+    requests and the state they are in.
+
+    The following ASCII Venn Diagram represents sets the related review requests
+    may be in and how they overlap.
+
+    Legend:
+
+    * "unpublished_rids" = squashed_rr.extra_data['p2rb.unpublished_rids']
+    * "discard_on_publish_rids" = squashed_rr.extra_data['p2rb.discard_on_publish_rids']
+    * "squashed.commits" = squashed_rr.extra_data['p2rb.commits']
+    * "draft.commits" = squashed_rr.draft.extra_data['p2rb.commits']
+
+    * A = unpublished_rids - draft.commits
+    * B = draft.commits - squashed.commits
+    * C = draft.commits - unpublished rids
+    * D = delete_on_publish_rids
+
+    Diagram::
+
+                unpublished_rids                       squashed.commits
+         ________________________________________________________________
+        |                             |                                  |
+        |                             |                                  |
+        |                _____________|_____________                     |
+        |               |             |             |                    |
+        |        A      |       draft.commits       |           D        |
+        |               |             |             |                    |
+        |               |             |             |                    |
+        |               |      B      |        C    |                    |
+        |               |             |             |                    |
+        |               |             |             |                    |
+        |               |_____________|_____________|                    |
+        |                             |                                  |
+        |                             |         discard_on_publish_rids  |
+        |                             |                                  |
+        |_____________________________|__________________________________|
+
+
+    The following rules should apply to the review request sets when publishing
+    or discarding.
+
+    When publishing the squashed review request:
+
+    * A: close "discarded" because it was never used
+    * B: publish draft
+    * C: publish draft
+    * D: close "discarded" because it is obsolete
+    * set unpublished_rids to empty '[]'
+    * set discard_on_publish_rids to empty '[]'
+
+    When discarding the draft of a published squashed review request:
+
+    * A: close "discarded" because it was never used (so it does not appear in
+      the owners dashboard)
+    * B: close "discarded" because it was never used (so it does not appear in
+      the owners dashboard)
+    * C: DELETE the review request draft
+    * D: do nothing
+    * set unpublished_rids to empty '[]'
+    * set discard_on_publish_rids to empty '[]'
+
+    When discarding an unpublished squashed review request (always a close "discarded"):
     """
     name = 'batch_review_request'
     allowed_methods = ('GET', 'POST',)
@@ -124,14 +238,15 @@ class BatchReviewRequestResource(WebAPIResource):
 
         try:
             with transaction.atomic():
-                squashed_rr = self._process_submission(
+                squashed_rr, node_to_rid, review_data, warnings = self._process_submission(
                     request, local_site, repo, identifier, commits)
-
                 return 200, {
                     self.item_result_key: {
+                        'nodes': node_to_rid,
                         'squashed_rr': squashed_rr.id,
-                    }
-                }
+                        'review_requests': review_data,
+                        'warnings': warnings,
+                    }}
 
         # Need to catch this outside the transaction so db changes are rolled back.
         except DiffProcessingException:
@@ -226,6 +341,10 @@ class BatchReviewRequestResource(WebAPIResource):
         # A mapping of review request id to dicts of additional metadata.
         review_data = {}
 
+        squashed_reviewers = set()
+
+        warnings = []
+
         # Do a pass and find all commits that map cleanly to old review requests.
         for commit in commits['individual']:
             node = commit['id']
@@ -269,8 +388,9 @@ class BatchReviewRequestResource(WebAPIResource):
                 unclaimed_rids.remove(rid)
 
                 rr = ReviewRequest.objects.get(pk=rid)
-                # TODO implement
-                #update_review_request(rr, commit)
+                draft, warns = update_review_request(local_site, request, rr, commit)
+                squashed_reviewers.update(u for u in draft.target_people.all())
+                warnings.extend(warns)
                 processed_nodes.add(node)
                 node_to_rid[node] = rid
                 review_requests[rid] = rr
@@ -283,7 +403,128 @@ class BatchReviewRequestResource(WebAPIResource):
 
                 break
 
-        return squashed_rr
+        # Now do a pass over the commits that didn't map cleanly.
+        for commit in commits['individual']:
+            node = commit['id']
+            if node in processed_nodes:
+                continue
+
+            # We haven't seen this commit before *and* our mapping above didn't
+            # do anything useful with it.
+
+            # This is where things could get complicated. We could involve
+            # heuristic based matching (comparing commit messages, changed
+            # files, etc). We may do that in the future.
+
+            # For now, match the commit up against the next one in the index.
+            # The unclaimed rids list contains review requests which were created
+            # when previously updating this review identifier, but not published.
+            # If we have more commits than were previously published we'll start
+            # reusing these private review requests before creating new ones.
+            if unclaimed_rids:
+                assumed_old_rid = unclaimed_rids.pop(0)
+                rr = ReviewRequest.objects.get(pk=assumed_old_rid)
+                draft, warns = update_review_request(local_site, request, rr, commit)
+                squashed_reviewers.update(u for u in draft.target_people.all())
+                warnings.extend(warns)
+                processed_nodes.add(commit['id'])
+                node_to_rid[node] = assumed_old_rid
+                review_requests[assumed_old_rid] = rr
+                review_data[assumed_old_rid] = get_review_request_data(rr)
+
+                try:
+                    discard_on_publish_rids.remove(assumed_old_rid)
+                except ValueError:
+                    pass
+
+                continue
+
+            # There are no more unclaimed review request IDs. This means we have
+            # more commits than before. Create new review requests as appropriate.
+            rr = ReviewRequest.objects.create(user=user,
+                                              repository=repo,
+                                              commit_id=None,
+                                              local_site=local_site)
+
+            rr.extra_data['p2rb'] = True
+            rr.extra_data['p2rb.is_squashed'] = False
+            rr.extra_data['p2rb.identifier'] = identifier
+            rr.save(update_fields=['extra_data'])
+            logger.info('created commit review request #%d' % rr.id)
+            draft, warns = update_review_request(local_site, request, rr, commit)
+            squashed_reviewers.update(u for u in draft.target_people.all())
+            warnings.extend(warns)
+            processed_nodes.add(commit['id'])
+            node_to_rid[node] = rr.id
+            review_requests[rr.id] = rr
+            review_data[rr.id] = get_review_request_data(rr)
+            unpublished_rids.append(rr.id)
+
+        # At this point every incoming commit has been accounted for.
+        # If there are any remaining review requests, they must belong to
+        # deleted commits. (Or, we made a mistake and updated the wrong review
+        # request)
+        for rid in unclaimed_rids:
+            rr = ReviewRequest.objects.get(pk=rid)
+
+            if rr.public and rid not in discard_on_publish_rids:
+                # This review request has already been published so we'll need to
+                # discard it when we publish the squashed review request.
+                discard_on_publish_rids.append(rid)
+            elif not rr.public and rid not in unpublished_rids:
+                # We've never published this review request so it may be reused in
+                # the future for *any* commit. Keep track of it.
+                unpublished_rids.append(rid)
+            else:
+                # This means we've already marked the review request properly
+                # in a previous push, so do nothing.
+                pass
+
+        commit_list = []
+        for commit in commits['individual']:
+            node = commit['id']
+            commit_list.append([node, node_to_rid[node]])
+
+        # We need to refresh the squashed rr and draft because post save hooks
+        # in ReviewBoard result in magical changes to some of its fields.
+        squashed_rr = ReviewRequest.objects.get(pk=squashed_rr.id)
+        squashed_draft = squashed_rr.draft.get()
+
+        squashed_draft.summary = identifier
+
+        # Reviewboard does not allow review requests with empty descriptions to
+        # be published, so we insert some filler here.
+        squashed_draft.description = 'This is the parent review request'
+        squashed_draft.bugs_closed = ','.join(sorted(set(commit['bug'] for commit in commits['individual'])))
+
+        squashed_draft.depends_on.clear()
+        for rrid in sorted(node_to_rid.values()):
+            rr = ReviewRequest.objects.for_id(rrid, local_site)
+            squashed_draft.depends_on.add(rr)
+
+        squashed_draft.target_people.clear()
+        for user in sorted(squashed_reviewers):
+            squashed_draft.target_people.add(user)
+
+        squashed_draft.extra_data['p2rb.commits'] = json.dumps(commit_list)
+
+        if 'base_commit_id' in commits['squashed']:
+            squashed_draft.extra_data['p2rb.base_commit'] = commits['squashed']['base_commit_id']
+
+        squashed_rr.extra_data.update({
+            'p2rb.discard_on_publish_rids': json.dumps(discard_on_publish_rids),
+            'p2rb.unpublished_rids': json.dumps(unpublished_rids),
+            'p2rb.first_public_ancestor': commits['squashed']['first_public_ancestor'],
+        })
+
+        squashed_draft.save()
+        squashed_rr.save(update_fields=['extra_data'])
+
+        review_requests[squashed_rr.id] = squashed_rr
+        review_data[squashed_rr.id] = get_review_request_data(squashed_rr)
+
+        return squashed_rr, node_to_rid, review_data, warnings
+
 
 batch_review_request_resource = BatchReviewRequestResource()
 
@@ -306,20 +547,22 @@ def update_diffset_history(rr, diffset):
         diffset.revision = 1
 
 
-def update_review_request_draft_diffset(rr, diffset):
+def update_review_request_draft_diffset(rr, diffset, draft=None):
     """Update the diffset on a review request draft.
 
     Given a ReviewRequest, obtain or create the draft and update the diffset
     on it.
     """
+    if not draft:
+        try:
+            draft = rr.draft.get()
+        except ReviewRequestDraft.DoesNotExist:
+            draft = ReviewRequestDraft.create(rr)
+
     discarded_diffset = None
 
-    try:
-        draft = rr.draft.get()
-        if draft.diffset and draft.diffset != diffset:
-            discarded_diffset = draft.diffset
-    except ReviewRequestDraft.DoesNotExist:
-        draft = ReviewRequestDraft.create(rr)
+    if draft.diffset and draft.diffset != diffset:
+        discarded_diffset = draft.diffset
 
     draft.diffset = diffset
     draft.save()
@@ -330,6 +573,124 @@ def update_review_request_draft_diffset(rr, diffset):
     return draft
 
 
+def previous_reviewers(rr):
+    """Return the result of the most recent review given by each reviewer"""
+
+    # TODO: Ideally this would get approval state for each reviewer as
+    # calculated in the MozReviewApprovalHook to be absolutely sure we're
+    # consistent.
+    pr = {}
+    for review in gen_latest_reviews(rr):
+        pr[review.user.username] = review.ship_it
+
+    return pr
+
+
+def get_user_from_reviewer(request, reviewer):
+    for backend in get_enabled_auth_backends():
+        backend.query_users(reviewer, request)
+
+    q = Q(username__icontains=reviewer)
+    q = q | Q(is_active=True)
+
+    users = User.objects.filter(q).all()
+    # Try exact username match.
+    for user in users:
+        if user.username == reviewer:
+            return user
+
+    # Try case insensitive username match.
+    for user in users:
+        if user.username.lower() == reviewer.lower():
+            return user
+
+    return None
+
+
+def resolve_reviewers(request, requested_reviewers):
+    reviewers = set()
+    unrecognized = set()
+    # TODO track mapping for multiple review requests and cache results.
+    for reviewer in requested_reviewers:
+        user = get_user_from_reviewer(request, reviewer)
+        if user:
+            if not any(u.username == user.username for u in reviewers):
+                reviewers.add(user)
+        else:
+            unrecognized.add(reviewer)
+
+    return reviewers, unrecognized
+
+
+def update_review_request(local_site, request, rr, commit):
+    """Synchronize the state of a review request with a commit.
+
+    Updates the commit message, refreshes the diff, etc.
+    """
+    try:
+        draft = rr.draft.get()
+    except ReviewRequestDraft.DoesNotExist:
+        draft = ReviewRequestDraft.create(rr)
+
+    draft.summary = commit['message'].splitlines()[0]
+    draft.description = commit['message']
+    draft.bugs_closed = commit['bug']
+    draft.extra_data['p2rb.commit_id'] = commit['id']
+    draft.extra_data['p2rb.first_public_ancestor'] = commit['first_public_ancestor']
+
+    reviewer_users, unrecognized_reviewers = \
+        resolve_reviewers(request, commit.get('reviewers', []))
+    requal_reviewer_users, unrecognized_requal_reviewers = \
+        resolve_reviewers(request, commit.get('requal_reviewers', []))
+
+    warnings = []
+
+    for reviewer in unrecognized_reviewers | unrecognized_requal_reviewers:
+        warnings.append('unrecognized reviewer: %s' % reviewer)
+        logger.info('unrecognized reviewer: %s' % reviewer)
+
+    if requal_reviewer_users:
+        pr = previous_reviewers(rr)
+        for user in requal_reviewer_users:
+            if not pr.get(user.username, False):
+                warnings.append('commit message for %s has r=%s but they '
+                                'have not granted a ship-it. review will be '
+                                'requested on your behalf' % (
+                                commit['id'][:12], user.username))
+
+        reviewer_users |= requal_reviewer_users
+
+    # Carry over from last time unless commit message overrules.
+    if reviewer_users:
+        draft.target_people.clear()
+    for user in sorted(reviewer_users):
+        draft.target_people.add(user)
+        logger.debug('adding reviewer %s to #%d' % (user.username, rr.id))
+
+    try:
+        diffset = DiffSet.objects.create_from_data(
+            repository=rr.repository,
+            diff_file_name='diff',
+            diff_file_contents=commit['diff'],
+            parent_diff_file_name='diff',
+            parent_diff_file_contents=commit.get('parent_diff'),
+            diffset_history=None,
+            basedir='',
+            request=request,
+            base_commit_id=commit.get('base_commit_id'),
+            save=True,
+        )
+        update_diffset_history(rr, diffset)
+        diffset.save()
+    except Exception:
+        logger.exeption('error processing diff')
+        raise DiffProcessingException()
+
+    update_review_request_draft_diffset(rr, diffset, draft=draft)
+
+    return draft, warnings
+
+
 def get_review_request_data(rr):
     """Obtain a dictionary containing review request metadata.
 
@@ -338,7 +699,7 @@ def get_review_request_data(rr):
     Some values may be unicode, not str.
     """
     rd = {
-        'status': rr.status,
+        'status': status_to_string(rr.status),
     }
 
     thing = rr
