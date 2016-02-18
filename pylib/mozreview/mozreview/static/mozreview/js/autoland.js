71d03e9100feb4c8145b82df831421fd7f3edeec
Dan Minor <dminor@mozilla.com>
2016-02-18 08:40:13 -0500

    mozreview: fix title for autoland button on discarded review (bug 1243530) r=mdoglio
    MozReview-Commit-ID: D9ZXDOPncnP
    
    --HG--
    extra : rebase_source : da09ee8f34bb2dc46020e926a91ecd2a35804b16

--- pylib/mozreview/mozreview/static/mozreview/js/autoland.js
+++ pylib/mozreview/mozreview/static/mozreview/js/autoland.js
@@ -265,7 +265,7 @@ $(document).on("mozreview_ready", function() {
   } else if (!MozReview.hasScmLevel3) {
     autoland_trigger.attr('title', 'You must have scm_level_3 access to land');
   } else if (!MozReview.reviewRequestPending) {
-    try_trigger.attr('title', 'You can not autoland from a closed review request');
+    autoland_trigger.attr('title', 'You can not autoland from a closed review request');
   } else {
     MozReview.parentReviewRequest.ready({
       error: function () {
