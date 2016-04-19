40eb87397ab46465eb8dc1b636f462d1bbd95ebe
Gregory Szorc <gps@mozilla.com>
2016-04-19 14:39:05 -0700

    scripts: teach find-hg-repos.py to filter by group owner
    MozReview-Commit-ID: DZlJsuMVnij
    
    --HG--
    extra : histedit_source : 68d16ab49182ce83b2bc39bbf5918c9b9784b966

--- scripts/find-hg-repos.py
+++ scripts/find-hg-repos.py
@@ -1,12 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 """Find Mercurial repositories under a specified path."""
 
+import argparse
+import grp
 import os
 import sys
+
 
 def find_hg_repos(path):
     for root, dirs, files in os.walk(path):
@@ -16,11 +19,28 @@ def find_hg_repos(path):
 
         dirs[:] = [d for d in dirs if d != '.hg']
 
-if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        print('usage: %s dir0 [dir1] ...')
-        sys.exit(1)
 
-    for d in sys.argv[1:]:
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--group', help='Group owner to search for')
+    parser.add_argument('paths', nargs='+')
+
+    args = parser.parse_args()
+
+    gid = None
+    if args.group:
+        try:
+            group = grp.getgrnam(args.group)
+            gid = group[2]
+        except KeyError:
+            print('group %s is not known' % args.group)
+            sys.exit(1)
+
+    for d in args.paths:
         for path in find_hg_repos(d):
+            if gid is not None:
+                st = os.stat(path)
+                if st.st_gid != gid:
+                    continue
+
             print(path[len(d):])
