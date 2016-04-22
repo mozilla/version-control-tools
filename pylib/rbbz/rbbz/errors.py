a5f7054b93b6e29bb93b56cfff6cbb352fdd5751
David Walsh <dwalsh@mozilla.com>
2016-04-22 12:40:58 -0500

    MozReview: Remove rbbz/errors.py (Bug 1262548). r=smacleod
    MozReview-Commit-ID: DDUkYYHcfNm
    
    --HG--
    extra : rebase_source : 4e65068affe99d9cce2331bc44bf156b8ec24859
    extra : histedit_source : a69cfa4d77976013e62cb87b4c19fc5a46ea0d75

--- pylib/rbbz/rbbz/errors.py
+++
@@ -1,8 +0,0 @@
-from reviewboard.reviews.errors import PublishError
-
-#
-# Review request errors.
-#
-class InvalidBugsError(PublishError):
-    def __init__(self):
-        PublishError.__init__(self, 'Exactly one bug ID must be provided.')
