bdb3af033ab65cde1c1b418816f9a3692ef97540
David Walsh <dwalsh@mozilla.com>
2016-05-16 11:34:56 -0500

    MozReview: More elegantly hide the 'New Request' menu option (Bug 1273189) . r=glob
    MozReview-Commit-ID: 5JA7TGmVVaq
    
    --HG--
    extra : rebase_source : a9510a20c625a55fa28d2c72292832b04a61773c

--- pylib/mozreview/mozreview/static/mozreview/js/default.js
+++ pylib/mozreview/mozreview/static/mozreview/js/default.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   // Hide "New Review Request" link
-  $('#navbar a[href="/r/new/"]').parent('li').hide();
+  $('#navbar a[href="/r/new/"]').parent('li').remove();
 
   // Fix logout url
   $("a[href='/account/logout/']").attr("href", "/mozreview/logout/");
