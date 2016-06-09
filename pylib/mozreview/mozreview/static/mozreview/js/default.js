309da6103d8e41d68b59bd14f4972a0b428c32b2
David Walsh <dwalsh@mozilla.com>
2016-06-09 14:38:43 -0500

    MozReview: Hide 'Close' links in review listing sidebar (Bug 1279014). r=mconley,mcote
    MozReview-Commit-ID: KTq4AJFGerz
    
    --HG--
    extra : rebase_source : 202485f711338dfc66239a0989b9b98b7d8c79ab

--- pylib/mozreview/mozreview/static/mozreview/js/default.js
+++ pylib/mozreview/mozreview/static/mozreview/js/default.js
@@ -5,4 +5,7 @@ $(document).ready(function() {
   // Fix logout url
   $("a[href='/account/logout/']").attr("href", "/mozreview/logout/");
   $("#accountnav li").css("visibility", "visible");
+
+  // Hide "Close Discarded" and "Close Submitted" in request listings
+  $('#page_sidebar').find('a.discard, a.submit').remove();
 });
