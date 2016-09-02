2b90cc6278240415299f4db79f8b22110dafa1b7
David Walsh <dwalsh@mozilla.com>
2016-09-02 13:27:48 -0500

    MozReview: Remove shims for hiding 'New Review Request' link (bug 1300158). r=glob
    MozReview-Commit-ID: Jjs0Ib9pFGm
    
    --HG--
    extra : rebase_source : f9a9e6adad10b4412725478cffbb48bb2a4288d0

--- pylib/mozreview/mozreview/static/mozreview/js/default.js
+++ pylib/mozreview/mozreview/static/mozreview/js/default.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-  // Hide "New Review Request" link
-  $('#navbar a[href="/r/new/"]').parent('li').remove();
 
   // Fix logout url
   $("a[href='/account/logout/']").attr("href", "/mozreview/logout/");
