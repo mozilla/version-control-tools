4e113cd25140a24a5061dd14f692d85c64355a5b
Mark Cote <mcote@mozilla.com>
2017-02-08 21:29:28 -0500

    testing: Fix autoland daemon logging (bug 1338022); r=glob
    The autoland daemon in the testing docker container was still using the
    old --log-path argument, which no longer exists.  Fixed to redirect stdout
    and stderr to the same log file.
    
    MozReview-Commit-ID: Kf2vpUlngDD
    
    --HG--
    extra : rebase_source : b56c85b51e830d88c5ba476ea74a90b44cf5b13e

--- testing/docker/builder-autoland/start-autoland.sh
+++ testing/docker/builder-autoland/start-autoland.sh
@@ -4,5 +4,5 @@ cd $AUTOLAND_HOME
 . venv/bin/activate
 
 cd autoland
-python autoland.py --log-path=/home/autoland/autoland.log --dsn="dbname=autoland user=postgres host=autolanddb" &
+(python autoland.py --dsn="dbname=autoland user=postgres host=autolanddb" >& /home/autoland/autoland.log) &
 apache2ctl -D FOREGROUND
