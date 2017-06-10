7bd6e3c303b1dd12895ca437526c49f6adbc65d8
Steve Fink <sfink@mozilla.com>
2017-06-09 17:50:41 -0700

    Bug 1371881 - Add --patch-id for manually obsoleting selected attachments, r=gps

--- hgext/bzexport/__init__.py
+++ hgext/bzexport/__init__.py
@@ -955,6 +955,8 @@ def patch_id(ui, repo, rev):
           'Interactive -- request confirmation before any permanent action'),
          ('', 'no-take-bug', False,
           'Do not assign bug to myself'),
+         ('', 'patch-id', '',
+          'Use the given name as the patch filename, obsoleting any patches with the same name'),
          ('', 'bug-description', '',
           'New bug description (aka comment 0)'),
          ('u', 'update', None,
@@ -1018,7 +1020,7 @@ def bzexport(ui, repo, *args, **opts):
             # Support older hg versions
             patch.export(repo, [rev], fp=contents, opts=diffopts)
 
-    filename = patch_id(ui, repo, rev)
+    filename = opts['patch_id'] or patch_id(ui, repo, rev)
 
     if opts['ignore_all_space']:
         filename += "_ws"
