f105f86066f0f4a2f6597243b14c98649ab37adb
Gregory Szorc <gps@mozilla.com>
2017-08-08 12:04:21 -0700

    mozhg: add setup.py; r=glob
    This is needed so we can install the package properly.
    
    MozReview-Commit-ID: 4i1z7rXnHEm
    
    --HG--
    extra : rebase_source : 573ae29c56e3423cc78ac54cac8c89f035310dd7

---
+++ pylib/mozhg/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='mozhg',
+    version='0.1',
+    description='Common Mercurial code to support Mozilla Mercurial '
+                'customizations',
+    author='Mozilla Developer Services',
+    packages=['mozhg'],
+)
