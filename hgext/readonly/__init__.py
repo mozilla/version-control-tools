defddf4bf2b4c25959f9be53ec1837d34cea9a6a
Gregory Szorc <gps@mozilla.com>
2018-01-31 00:26:16 -0800

    readonly: mark as compatible with Mercurial 4.4 (bug 1434091); r=glob
    All tests pass with Mercurial 4.3 and 4.4. So mark the extension as
    compatible.
    
    Differential Revision: https://phabricator.services.mozilla.com/D502
    
    --HG--
    extra : rebase_source : 9c47a1a5aca513ef85bdf12a308f69ba7db4f2dc
    extra : amend_source : df15081a00f3fe8a10f61dc43ec4389ff0620a49
    extra : histedit_source : 774c754666ef4df1ceffda971217f17ab6606b0e

--- hgext/readonly/__init__.py
+++ hgext/readonly/__init__.py
@@ -24,7 +24,7 @@ from mercurial import (
     util,
 )
 
-testedwith = '4.1 4.2'
+testedwith = '4.1 4.2 4.3 4.4'
 minimumhgversion = '4.1'
 buglink = 'https://bugzilla.mozilla.org/enter_bug.cgi?product=Developer%20Services&component=Mercurial%3A%20hg.mozilla.org'
 
