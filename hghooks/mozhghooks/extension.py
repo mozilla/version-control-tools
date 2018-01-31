c08081d62d7816aede03e7094fde0c4a849ba94b
Gregory Szorc <gps@mozilla.com>
2018-01-31 09:37:47 -0800

    hghooks: register config options (bug 1434069); r=glob
    This is needed to avoid developer warnings in Mercurial 4.4.
    
    Differential Revision: https://phabricator.services.mozilla.com/D487
    
    --HG--
    extra : rebase_source : 82145eed282193c85b4293476de7c21a51663ab3
    extra : amend_source : 16d8e79147786b7af4436132d289339af32609dd
    extra : histedit_source : ae08049fd5313e8d4f5b9e276d01b99d355938b2

--- hghooks/mozhghooks/extension.py
+++ hghooks/mozhghooks/extension.py
@@ -5,6 +5,16 @@
 
 from __future__ import absolute_import
 
+from mercurial import (
+    registrar,
+    util,
+)
+
+try:
+    from mercurial import configitems
+except ImportError:
+    configitems = None
+
 from mozhg.util import (
     identify_repo,
 )
@@ -12,6 +22,26 @@ from mozhg.util import (
 testedwith = '4.2 4.3'
 minimumhgversion = '4.2'
 buglink = 'https://bugzilla.mozilla.org/enter_bug.cgi?product=Developer%20Services&component=Mercurial%3A%20hg.mozilla.org'
+
+# TRACKING hg43 Mercurial 4.3 introduced the config registrar. 4.4
+# requires config items to be registered to avoid a devel warning.
+if util.safehasattr(registrar, 'configitem'):
+    configtable = {}
+    configitem = registrar.configitem(configtable)
+
+    # TRACKING hg44 generic argument not added until 4.4.
+    try:
+        configitem('allowedroots', '.*',
+                   generic=True)
+        configitem('mozilla', 'check.*',
+                   generic=True)
+    except TypeError as e:
+        pass
+
+    configitem('mozilla', 'repo_root',
+               default=configitems.dynamicdefault)
+    configitem('mozilla', 'treeherder_repo',
+               default=None)
 
 
 def get_check_classes(hook):
