c21c3ee3722dc32e5204f4025b1b09b1e666d99e
Connor Sheehan <sheehan@mozilla.com>
2018-05-14 17:56:59 +0000

    mozext: mark as compatible with hg46 (Bug 1461160) r=gps
    Forgot to add this line to my previous patch
    
    Differential Revision: https://phabricator.services.mozilla.com/D1278

--- hgext/mozext/__init__.py
+++ hgext/mozext/__init__.py
@@ -346,7 +346,7 @@ with demandimport.deactivated():
 
 bz_available = False
 
-testedwith = '4.2 4.3 4.4 4.5'
+testedwith = '4.2 4.3 4.4 4.5 4.6'
 minimumhgversion = '4.2'
 buglink = 'https://bugzilla.mozilla.org/enter_bug.cgi?product=Developer%20Services&component=Mercurial%3A%20mozext'
 
