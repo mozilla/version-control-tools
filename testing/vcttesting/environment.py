7e2b4659419042ee05d65362b331eb5ecfb7d88d
Connor Sheehan <sheehan@mozilla.com>
2018-07-27 18:09:56 +0000

    testing: stop installing legacy Mercurial versions in test environment r=gps
    The Developer Workflow team officially supports Mercurial versions
    greater than or equal to 4.3. Let's stop installing any version
    less than 4.3 while creating the test environment.
    
    Differential Revision: https://phabricator.services.mozilla.com/D2472
    
    --HG--
    extra : moz-landing-system : lando

--- testing/vcttesting/environment.py
+++ testing/vcttesting/environment.py
@@ -101,10 +101,6 @@ def install_editable(venv, relpath, extra_env=None):
 def install_mercurials(venv, hg):
     """Install supported Mercurial versions in a central location."""
     VERSIONS = [
-        '3.9.2',
-        '4.0.2',
-        '4.1.2',
-        '4.2.3',
         '4.3.3',
         '4.4.2',
         '4.5.3',
