5d06c9e83edda8b846ca3fd0dc03737a384e3725
Connor Sheehan <sheehan@mozilla.com>
2018-10-30 17:29:04 +0000

    robustcheckout: remove incompatible versions marked as compatible r=gps
    The `supported_hg` check requires versions 4.3+, yet
    the `testedwith` and `minimumhgversion` strings reference
    versions as old as 3.7. Might as well make those accurate.
    
    Differential Revision: https://phabricator.services.mozilla.com/D10265
    
    --HG--
    extra : moz-landing-system : lando

--- hgext/robustcheckout/__init__.py
+++ hgext/robustcheckout/__init__.py
@@ -49,8 +49,8 @@ except ImportError:
 # Causes worker to purge caches on process exit and for task to retry.
 EXIT_PURGE_CACHE = 72
 
-testedwith = '3.7 3.8 3.9 4.0 4.1 4.2 4.3 4.4 4.5 4.6 4.7 4.8'
-minimumhgversion = '3.7'
+testedwith = '4.3 4.4 4.5 4.6 4.7 4.8'
+minimumhgversion = '4.3'
 
 cmdtable = {}
 
