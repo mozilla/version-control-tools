af33cdb9eb0d48f1b87d286596277a5a4f75ee54
Gregory Szorc <gps@mozilla.com>
2018-11-28 17:26:18 +0000

    hgwsgi: set HTTPS_PROXY from /etc/environment (bug 1509943); r=sheehan
    We could have potentially done this with SetEnv in httpd.conf or by
    sourcing /etc/environment in the httpd systemd unit. However, because
    /etc/environment defines extra variables that are not relevant and
    one of those - HTTP_PROXY - could confuse WSGI, we want to only
    apply what we need.
    
    Reading the Internets, apparently some WSGI servers strip environment
    variables coming from the parent process. So setting a value in SetEnv
    may not work.
    
    In addition, each server may have its own proxy server. And teaching
    Ansible to parse the /etc/environment file or to define per-server
    proxies feels like a bit of work. Minimal Python code in the shared
    bootstrap file gets the job done.
    
    Differential Revision: https://phabricator.services.mozilla.com/D13165
    
    --HG--
    extra : moz-landing-system : lando

--- hgwsgi/bootstrap.py
+++ hgwsgi/bootstrap.py
@@ -4,15 +4,41 @@
 # This file contains common code that is executed by every hgweb.wsgi
 # WSGI entrypoint.
 
+import errno
 import os
 
+# Set this before importing mercurial.* modules.
 os.environ['HGENCODING'] = 'UTF-8'
 
 
 from mercurial.hgweb import hgweb
 
 
+# Set HTTPS_PROXY from /etc/environment value, if present. We don't
+# source /etc/environment completely because we have no need for
+# some of its variables. And HTTP_PROXY could confuse WSGI into
+# thinking the client sent a "Proxy: " request header.
+def set_env():
+    try:
+        with open('/etc/environment', 'rb') as fh:
+            for line in fh:
+                if not line.startswith(b'HTTPS_PROXY='):
+                    continue
+
+                value = line.strip().split(b'=', 1)[1]
+                value = value.strip('"')
+
+                os.environ['HTTPS_PROXY'] = value
+                break
+
+    except IOError as e:
+        if e.errno != errno.ENOENT:
+            raise
+
+
 def make_application(wsgi_dir):
+    set_env()
+
     config = os.path.join(wsgi_dir, 'hgweb.config')
 
     return hgweb(config)
