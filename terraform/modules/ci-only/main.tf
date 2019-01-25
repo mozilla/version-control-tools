806f705a90e09e1d13de8e89a306f26b192eeb30
Connor Sheehan <sheehan@mozilla.com>
2019-01-25 17:01:16 +0000

    terraform: create load balancer target group and listener (Bug 1516029) r=bstack
    Now that we have our domain and SSL certificate set up, we can
    attach our resources to the internal load balancer. This commit
    creates a load balancer target group for our mirror hosts and
    adds a listener which forwards https requests from our load balancer
    to the group.
    
    Differential Revision: https://phabricator.services.mozilla.com/D17653
    
    --HG--
    extra : moz-landing-system : lando

--- terraform/modules/ci-only/main.tf
+++ terraform/modules/ci-only/main.tf
@@ -240,6 +240,32 @@ resource "aws_lb" "internal-lb" {
   }
 }
 
+resource "aws_lb_target_group" "http-mirror-target-group" {
+  name = "${data.aws_region.current.name}-hg-mirrors"
+  port = 80
+  protocol = "HTTP"
+  vpc_id = "${aws_vpc.hgci-vpc.id}"
+}
+
+resource "aws_lb_target_group_attachment" "mirror-attachment" {
+  target_group_arn = "${aws_lb_target_group.http-mirror-target-group.arn}"
+  target_id = "${module.test-hgweb-mirror.instance_id}"
+  port = 80
+}
+
+resource "aws_lb_listener" "mirror-https-listener" {
+  load_balancer_arn = "${aws_lb.internal-lb.arn}"
+  port = 443
+  protocol = "HTTPS"
+  ssl_policy = "ELBSecurityPolicy-2016-08"
+  certificate_arn = "${var.certificate_arn}"
+
+  default_action {
+    type = "forward"
+    target_group_arn = "${aws_lb_target_group.http-mirror-target-group.arn}"
+  }
+}
+
 module "test-hgweb-mirror" {
   source = "../hgweb-mirror"
 
