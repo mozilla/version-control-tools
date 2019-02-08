4039747203495f55da36917b73123bc094064704
Connor Sheehan <sheehan@mozilla.com>
2019-02-08 12:04:45 -0500

    terraform: update to newest CentOS7 AMIs
    The old AMI we were using only include cloud-init v0.7.2.
    We require at least v18.2 for the `instance-data.json`
    file to be written to disk.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,8 +6,9 @@ variable "centos7_amis" {
     description = "Centos 7 AMI IDs for various AWS regions"
     type = "map"
     default = {
-        us-east-1 = "ami-6d1c2007"
-        us-west-1 = "ami-af4333cf"
-        us-west-2 = "ami-3ecc8f46"
+        us-east-1 = "ami-02eac2c0129f6376b"
+        us-east-2 = "ami-0f2b4fc905b0bd1f1"
+        us-west-1 = "ami-074e2d6769f445be5"
+        us-west-2 = "ami-01ed306a12b7d1c96"
     }
 }
