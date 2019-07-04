4b1b206ce0af9e169c094861d7c89ad70f392ccc
Connor Sheehan <sheehan@mozilla.com>
2019-07-04 17:43:10 +0000

    push-to-try: add byte string prefixes (Bug 1563138) r=smacleod
    This commit adds byte-string prefixes to all necessary strings
    in the `push-to-try` extension.
    
    Differential Revision: https://phabricator.services.mozilla.com/D36708
    
    --HG--
    extra : moz-landing-system : lando

--- hgext/push-to-try/__init__.py
+++ hgext/push-to-try/__init__.py
@@ -11,8 +11,8 @@ from mercurial import (
 )
 from mercurial.i18n import _
 
-OUR_DIR = os.path.normpath(os.path.dirname(__file__))
-with open(os.path.join(OUR_DIR, '..', 'bootstrap.py')) as f:
+OUR_DIR = os.path.normpath(os.path.dirname(__file__)).encode('ascii')
+with open(os.path.join(OUR_DIR, b'..', b'bootstrap.py')) as f:
     exec(f.read())
 
 from mozhg.rewrite import preservefilectx
@@ -24,48 +24,48 @@ command = registrar.command(cmdtable)
 configtable = {}
 configitem = registrar.configitem(configtable)
 
-configitem('push-to-try', 'nodate',
+configitem(b'push-to-try', b'nodate',
            default=False)
 
-testedwith = '4.6 4.7 4.8 4.9 5.0'
-minimumhgversion = '4.6'
+testedwith = b'4.6 4.7 4.8 4.9 5.0'
+minimumhgversion = b'4.6'
 
-@command('push-to-try', [
-    ('m', 'message', '', 'commit message to use', 'MESSAGE'),
-    ('s', 'server', '', 'push destination', 'URL'),
-], '-m MESSAGE -s URL')
+@command(b'push-to-try', [
+    (b'm', b'message', b'', b'commit message to use', b'MESSAGE'),
+    (b's', b'server', b'', b'push destination', b'URL'),
+], b'-m MESSAGE -s URL')
 def push_to_try(ui, repo, server, message=None):
 
-    nodate = ui.configbool('push-to-try', 'nodate')
+    nodate = ui.configbool(b'push-to-try', b'nodate')
 
     if not server:
-        if 'try' in ui.paths:
-            server = 'try'
+        if b'try' in ui.paths:
+            server = b'try'
         else:
-            server = 'ssh://hg.mozilla.org/try'
+            server = b'ssh://hg.mozilla.org/try'
 
     if not message:
-        ui.status("STOP! A commit message is required.\n")
+        ui.status(b"STOP! A commit message is required.\n")
         return
 
     cctx = context.workingctx(repo)
-    if 'try_task_config.json' not in cctx and 'try:' not in message:
-        ui.status("STOP! Either try_task_config.json must be added or the commit "
-                  "message must contain try syntax.\n")
+    if b'try_task_config.json' not in cctx and b'try:' not in message:
+        ui.status(b"STOP! Either try_task_config.json must be added or the commit "
+                  b"message must contain try syntax.\n")
         return
 
-    if 'try_task_config.json' in cctx:
-        data = repo.wvfs.tryread('try_task_config.json')
+    if b'try_task_config.json' in cctx:
+        data = repo.wvfs.tryread(b'try_task_config.json')
         try:
             # data could be an empty string if tryread failed, which will
             # produce a ValueError here.
             data = json.loads(data)
         except ValueError as e:
-            ui.status("Error reading try_task_config.json: %s\n" % e.message)
+            ui.status(b"Error reading try_task_config.json: %s\n" % e.message)
             return
 
     # Invent a temporary commit with our message.
-    ui.status("Creating temporary commit for remote...\n")
+    ui.status(b"Creating temporary commit for remote...\n")
     status = repo.status()
     if status.modified + status.added + status.removed:
         # TODO: Achieve this by re-using the status call above to avoid the
@@ -83,12 +83,12 @@ def push_to_try(ui, repo, server, message=None):
                           message,
                           cctx.files(),
                           mk_memfilectx,
-                          date="0 0" if nodate else None)
+                          date=b"0 0" if nodate else None)
 
     # These messages are expected when we abort our transaction, but aren't
     # helpful to a user and may be misleading so we surpress them here.
-    filtered_phrases = {_("transaction abort!\n"),
-                        _("rollback completed\n")}
+    filtered_phrases = {_(b"transaction abort!\n"),
+                        _(b"rollback completed\n")}
     def filtered_warn(*msgs, **opts):
         if msgs:
             filtered = [m for m in msgs if m not in filtered_phrases]
@@ -98,11 +98,11 @@ def push_to_try(ui, repo, server, message=None):
     lock = tr = None
     try:
         lock = repo.lock()
-        tr = repo.transaction('push-to-try', report=filtered_warn)
+        tr = repo.transaction(b'push-to-try', report=filtered_warn)
         m = mctx.commit()
         # Push to try.
         commands.push(ui, repo, server, force=True, rev=[repo[m].rev()])
-        ui.status('push complete\n')
+        ui.status(b'push complete\n')
         # And rollback to the previous state.
         tr.abort()
     finally:
@@ -110,4 +110,4 @@ def push_to_try(ui, repo, server, message=None):
             tr.release()
         if lock:
             lock.release()
-        ui.status("temporary commit removed, repository restored\n")
+        ui.status(b"temporary commit removed, repository restored\n")
