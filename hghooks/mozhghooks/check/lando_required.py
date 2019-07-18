6db7dd9a9fcfa21546788ba69ed4796bc00c755a
Connor Sheehan <sheehan@mozilla.com>
2019-07-18 17:23:29 +0000

    hghooks: move hard-coded string constants to top level (Bug 1515119) r=glob
    These strings are not required to be class-level constant variables,
    and their presence within the check class has no functional value.
    Leaving them in the class requires prefixing `self.` to each use
    of the variable. This commit moves all the variables to the top
    level and removes many uses of `self`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D38350
    
    --HG--
    extra : moz-landing-system : lando

--- hghooks/mozhghooks/check/lando_required.py
+++ hghooks/mozhghooks/check/lando_required.py
@@ -28,55 +28,62 @@ import sentry_sdk
 
 from hgmolib.ldap_helper import get_active_scm_groups
 
+MAGIC_WORDS = "MANUAL PUSH:"
+MAGICWORDS_WITH_JUSTIFICATION_RE = re.compile(
+    r".*(%s)\s*(.+)" % re.escape(MAGIC_WORDS)
+)
+
+ACTIVE_SCM_ALLOW_DIRECT_PUSH = "active_scm_allow_direct_push"
+
+ACTIVE_SCM_LEVEL_3 = "active_scm_level_3"
+
+SUCCESS_FOR_SCM_ALLOW_DIRECT_PUSH_LOG_MESSAGE = (
+    "Successful push: %s by %s (ACTIVE_SCM_ALLOW_DIRECT_PUSH)"
+)
+
+SUCCESS_FOR_SCM_LEVEL_3_LOG_MESSAGE = (
+    "Successful push: %s by %s (ACTIVE_SCM_LEVEL_3)"
+)
+
+SUBMIT_BUGZILLA_URL = "<https://mzl.la/2HX9Te2>"
+
+SCM_LEVEL_3_PUSH_ERROR_MESSAGE = (
+    "Pushing directly to this repo is disallowed, please use Lando.\n"
+    'To override, in your top commit, include the literal string, "%s",\n'
+    "followed by a sentence of justification."
+) % MAGIC_WORDS
+
+SENTRY_FAILURE_WARNING_MESSAGE = (
+    "WARNING: connecting or pushing to Sentry has failed, reporting:\n"
+    "LandoRequiredCheck: %%s\n"
+    "Please report this message by filing a bug at %s.\n"
+    "You do NOT need to retry as a result of this error message. Continuing...\n"
+) % SUBMIT_BUGZILLA_URL
+
+LDAP_USER_FAILURE_MESSAGE = (
+    "Unable to retrieve LDAP information about you, therefore we cannot allow\n"
+    "your push to proceed. This is a fatal error.\n"
+    "You may retry your push in the hopes that this a transient problem.\n"
+    "If this problem persists, please report this error by filing a bug at %s\n"
+) % SUBMIT_BUGZILLA_URL
+
+LDAP_USER_EXCEPTION_FAILURE_MESSAGE = (
+    "%s" "Please include this information in your bug submission:\n%%s"
+) % LDAP_USER_FAILURE_MESSAGE
+
+INSUFFICIENT_PRIVILEGE_FAILURE_MESSAGE = (
+    "You do not have sufficient privilege to push to this repo.\n"
+)
+
+INTERNAL_ERROR_MESSAGE = (
+    "An internal error has prevented you from successfully pushing.\n"
+    "You may retry your push in the hopes that this a transient problem.\n"
+    "If this problem persists, please report this error by filing a bug at %s\n"
+    'Include "LandoRequiredCheck: invalid privilege_level" in your error report\n'
+) % SUBMIT_BUGZILLA_URL
+
 
 class LandoRequiredCheck(PreTxnChangegroupCheck):
-
-    MAGIC_WORDS = "MANUAL PUSH:"
-    MAGICWORDS_WITH_JUSTIFICATION_RE = re.compile(
-        r".*(%s)\s*(.+)" % re.escape(MAGIC_WORDS)
-    )
-
-    ACTIVE_SCM_ALLOW_DIRECT_PUSH = "active_scm_allow_direct_push"
-    ACTIVE_SCM_LEVEL_3 = "active_scm_level_3"
-
-    SUCCESS_FOR_SCM_ALLOW_DIRECT_PUSH_LOG_MESSAGE = (
-        "Successful push: %s by %s (ACTIVE_SCM_ALLOW_DIRECT_PUSH)"
-    )
-    SUCCESS_FOR_SCM_LEVEL_3_LOG_MESSAGE = (
-        "Successful push: %s by %s (ACTIVE_SCM_LEVEL_3)"
-    )
-
-    SUBMIT_BUGZILLA_URL = "<https://mzl.la/2HX9Te2>"
-
-    SCM_LEVEL_3_PUSH_ERROR_MESSAGE = (
-        "Pushing directly to this repo is disallowed, please use Lando.\n"
-        'To override, in your top commit, include the literal string, "%s",\n'
-        "followed by a sentence of justification."
-    ) % MAGIC_WORDS
-    SENTRY_FAILURE_WARNING_MESSAGE = (
-        "WARNING: connecting or pushing to Sentry has failed, reporting:\n"
-        "PriviligedPushHook: %%s\n"
-        "Please report this message by filing a bug at %s.\n"
-        "You do NOT need to retry as a result of this error message. Continuing...\n"
-    ) % SUBMIT_BUGZILLA_URL
-    LDAP_USER_FAILURE_MESSAGE = (
-        "Unable to retrieve LDAP information about you, therefore we cannot allow\n"
-        "your push to proceed. This is a fatal error.\n"
-        "You may retry your push in the hopes that this a transient problem.\n"
-        "If this problem persists, please report this error by filing a bug at %s\n"
-    ) % SUBMIT_BUGZILLA_URL
-    LDAP_USER_EXCEPTION_FAILURE_MESSAGE = (
-        "%s" "Please include this information in your bug submission:\n%%s"
-    ) % LDAP_USER_FAILURE_MESSAGE
-    INSUFFICIENT_PRIVILEGE_FAILURE_MESSAGE = (
-        "You do not have sufficient privilege to push to this repo.\n"
-    )
-    INTERNAL_ERROR_MESSAGE = (
-        "An internal error has prevented you from successfully pushing.\n"
-        "You may retry your push in the hopes that this a transient problem.\n"
-        "If this problem persists, please report this error by filing a bug at %s\n"
-        'Include "PrivilegedPushCheck: invalid privilege_level" in your error report\n'
-    ) % SUBMIT_BUGZILLA_URL
 
     @property
     def name(self):
@@ -109,7 +116,7 @@ class LandoRequiredCheck(PreTxnChangegroupCheck):
                 # users' intent on pushing.  We have nowhere to log the failure, so we notify the
                 # user with a warning and proceed as if nothing bad had happened.
                 print_banner(
-                    self.ui, "warning", self.SENTRY_FAILURE_WARNING_MESSAGE % repr(e)
+                    self.ui, "warning", SENTRY_FAILURE_WARNING_MESSAGE % repr(e)
                 )
         else:
             # the sentry_dsn was an empty string - write to stdout instead of using sentry
@@ -132,7 +139,7 @@ class LandoRequiredCheck(PreTxnChangegroupCheck):
             False - the magic words and/or justification are not present in the description
             True - the magic words and justification are present and acceptable
         """
-        result = self.MAGICWORDS_WITH_JUSTIFICATION_RE.search(description)
+        result = MAGICWORDS_WITH_JUSTIFICATION_RE.search(description)
         if result is None:
             return False
         try:
@@ -164,23 +171,23 @@ class LandoRequiredCheck(PreTxnChangegroupCheck):
             # Since this method `pre` cannot react to fatal errors, the `None` value in
             # `privilege_level` will abort this check in the future call to method `check`
             print_banner(
-                self.ui, "error", self.LDAP_USER_EXCEPTION_FAILURE_MESSAGE % repr(e)
+                self.ui, "error", LDAP_USER_EXCEPTION_FAILURE_MESSAGE % repr(e)
             )
             return
         if not self.user_groups:
             # Since this method `pre` cannot react to fatal errors, the `None` value in
             # `privilege_level` will abort this check in the future call to method `check`
-            print_banner(self.ui, "error", self.LDAP_USER_FAILURE_MESSAGE)
+            print_banner(self.ui, "error", LDAP_USER_FAILURE_MESSAGE)
             return
-        if self.ACTIVE_SCM_ALLOW_DIRECT_PUSH in self.user_groups:
-            self.privilege_level = self.ACTIVE_SCM_ALLOW_DIRECT_PUSH
-        elif self.ACTIVE_SCM_LEVEL_3 in self.user_groups:
-            self.privilege_level = self.ACTIVE_SCM_LEVEL_3
+        if ACTIVE_SCM_ALLOW_DIRECT_PUSH in self.user_groups:
+            self.privilege_level = ACTIVE_SCM_ALLOW_DIRECT_PUSH
+        elif ACTIVE_SCM_LEVEL_3 in self.user_groups:
+            self.privilege_level = ACTIVE_SCM_LEVEL_3
         else:
             # neither ACTIVE_SCM_ALLOW_DIRECT_PUSH nor ACTIVE_SCM_LEVEL_3
             # Since this method `pre` cannot react to fatal errors, the `None` value in
             # `privilege_level` will abort this check in the future call to method `check`
-            print_banner(self.ui, "error", self.INSUFFICIENT_PRIVILEGE_FAILURE_MESSAGE)
+            print_banner(self.ui, "error", INSUFFICIENT_PRIVILEGE_FAILURE_MESSAGE)
 
     def check(self, ctx):
         """This method is called once for each of the commits within the changegroup
@@ -194,38 +201,38 @@ class LandoRequiredCheck(PreTxnChangegroupCheck):
             return False
         if self.first_ctx_rev is None:
             self.first_ctx_rev = ctx.hex()[:12]
-        if self.privilege_level is self.ACTIVE_SCM_ALLOW_DIRECT_PUSH:
+        if self.privilege_level is ACTIVE_SCM_ALLOW_DIRECT_PUSH:
             return True
-        if self.privilege_level is self.ACTIVE_SCM_LEVEL_3:
+        if self.privilege_level is ACTIVE_SCM_LEVEL_3:
             # MAGIC_WORDS and a justification must be in the last commit in the changesetgroup
             if len(ctx.children()) == 0:
                 # This is the last commit within a collection of changesets
                 # Test it for inclusion of MAGIC_WORDS and justification
                 if self._has_justification(ctx.description()):
                     return True
-                print_banner(self.ui, "error", self.SCM_LEVEL_3_PUSH_ERROR_MESSAGE)
+                print_banner(self.ui, "error", SCM_LEVEL_3_PUSH_ERROR_MESSAGE)
                 return False
             # this isn't the last commit in the changegroup. Just accept it.
             return True
         # this is some bad internal state where `privilege_level` is outside its allowed values.
-        print_banner(self.ui, "error", self.INTERNAL_ERROR_MESSAGE)
+        print_banner(self.ui, "error", INTERNAL_ERROR_MESSAGE)
         return False
 
     def post_check(self):
-        if self.privilege_level is self.ACTIVE_SCM_ALLOW_DIRECT_PUSH:
+        if self.privilege_level is ACTIVE_SCM_ALLOW_DIRECT_PUSH:
             self._log_push_attempt(
-                self.SUCCESS_FOR_SCM_ALLOW_DIRECT_PUSH_LOG_MESSAGE
+                SUCCESS_FOR_SCM_ALLOW_DIRECT_PUSH_LOG_MESSAGE
                 % (self.first_ctx_rev, self.user_name)
             )
             return True
-        if self.privilege_level is self.ACTIVE_SCM_LEVEL_3:
+        if self.privilege_level is ACTIVE_SCM_LEVEL_3:
             self._log_push_attempt(
-                self.SUCCESS_FOR_SCM_LEVEL_3_LOG_MESSAGE
+                SUCCESS_FOR_SCM_LEVEL_3_LOG_MESSAGE
                 % (self.first_ctx_rev, self.user_name)
             )
             return True
         # this is some bad internal state. At this point, `privilege_level` should have only been
         # one of the two allowed values above.  Getting to this point indicates an unexpected value
         # that should not happen.  Give an appropriate error message and abort.
-        print_banner(self.ui, "error", self.INTERNAL_ERROR_MESSAGE)
+        print_banner(self.ui, "error", INTERNAL_ERROR_MESSAGE)
         return False
