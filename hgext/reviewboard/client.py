226f3a6ca51b6ec653d5ff73e12e7b486fd7b43d
Connor Sheehan <sheehan@mozilla.com>
2019-11-06 14:43:34 +0000

    hgext/reviewboard: delete reviewboard extension directory (Bug 1486569) r=glob
    This was left in-place to warn users that they should delete the extension
    and move to Phabricator. It can safely be removed now.
    
    Differential Revision: https://phabricator.services.mozilla.com/D52004
    
    --HG--
    extra : moz-landing-system : lando

--- hgext/reviewboard/client.py
+++
@@ -1,9 +0,0 @@
-# This software may be used and distributed according to the terms of the
-# GNU General Public License version 2 or any later version.
-
-"""Review Board client extension."""
-
-def extsetup(ui):
-    ui.warn('the MozReview service has been disabled; '
-            'stop loading the reviewboard/mozreview extension from your hgrc '
-            'files to make this warning go away\n')
--- hgext/reviewboard/tests/test-extension-warning.t
+++
@@ -1,6 +0,0 @@
-  $ cat >> $HGRCPATH << EOF
-  > [extensions]
-  > reviewboard = $TESTDIR/hgext/reviewboard/client.py
-  > EOF
-  $ hg init foo
-  the MozReview service has been disabled; stop loading the reviewboard/mozreview extension from your hgrc files to make this warning go away
