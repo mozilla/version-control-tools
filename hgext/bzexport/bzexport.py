d20a060d99fb1e20b0ae24815894b5fc7ecca8d0
Mitchell Hentges <mhentges@mozilla.com>
2019-11-29 18:31:28 +0000

    bzext: removes bzpost and bzexport r=smacleod,sheehan
    Since we aren't publishing diffs to bugzilla for review anymore (we
    use Phabricator), we don't need these push-to-bugzilla extensions
    anymore.
    
    Differential Revision: https://phabricator.services.mozilla.com/D55038
    
    --HG--
    extra : moz-landing-system : lando

--- hgext/bzexport/bzexport.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os.path
-from mercurial import extensions
-
-# Redirect to __init__.py
-myfile = sys.modules['hgext_bzexport'].__file__
-extensions.loadpath(os.path.dirname(myfile), 'hgext.bzexport')
