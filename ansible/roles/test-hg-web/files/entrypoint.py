b61726720d8e01df2cabcc9551cfc2955cf7811f
Connor Sheehan <sheehan@mozilla.com>
2020-03-12 16:48:27 +0000

    ansible/test-hg-web: remove unnecessary hard-coding of `hgssh` string in the startup phase (Bug 1600827) r=mhentges,zeid
    Some more code that runs a string replacement of some value to `hgssh`.
    This commit moves the hard-coded string into the docker image by
    adding it as a step in the `test-hg-web` Ansible role.
    
    Differential Revision: https://phabricator.services.mozilla.com/D66457
    
    --HG--
    extra : moz-landing-system : lando

--- ansible/roles/test-hg-web/files/entrypoint.py
+++ ansible/roles/test-hg-web/files/entrypoint.py
@@ -12,7 +12,6 @@ import sys
 sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
 sys.stderr = sys.stdout
 
-ssh_hostname = 'hgssh'
 hostname = socket.gethostname()
 
 if not os.path.exists('/etc/ssh/ssh_host_ed25519_key'):
@@ -23,17 +22,6 @@ if not os.path.exists('/etc/ssh/ssh_host_rsa_key'):
     subprocess.check_call(['/usr/bin/ssh-keygen', '-t', 'rsa', '-b', '2048',
                            '-f', '/etc/ssh/ssh_host_rsa_key', '-N', ''])
 
-REPLACEMENTS = {
-    '@mirror_source@': ssh_hostname,
-}
-
-mirror_pull = open('/usr/local/bin/mirror-pull', 'rb').readlines()
-with open('/usr/local/bin/mirror-pull', 'wb') as fh:
-    for line in mirror_pull:
-        for k, v in REPLACEMENTS.items():
-            line = line.replace(k, v)
-
-        fh.write(line)
 
 REWRITE_CONSUMER_CONFIGS = (
     '/etc/mercurial/vcsreplicator.ini',
@@ -44,9 +32,6 @@ for f in REWRITE_CONSUMER_CONFIGS:
     vcsreplicator = open(f, 'rb').readlines()
     with open(f, 'wb') as fh:
         for line in vcsreplicator:
-            for k, v in REPLACEMENTS.items():
-                line = line.replace(k, v)
-
             # The client config file defines the client ID and group, which are
             # used for offset management. Since the file contents come from
             # the same container, we need to update the per-container values
@@ -59,20 +44,9 @@ for f in REWRITE_CONSUMER_CONFIGS:
 
             fh.write(line)
 
-# Replace SSH config for master server with the current environment's.
-ssh_config = open('/home/hg/.ssh/config', 'rb').readlines()
-with open('/home/hg/.ssh/config', 'wb') as fh:
-    for line in ssh_config:
-        if line.startswith('Host hg.mozilla.org'):
-            line = 'Host %s\n' % ssh_hostname
-        elif line.startswith('    Hostname'):
-            line = '    #%s' % line
-
-        fh.write(line)
-
 # Grab SSH host key from master. We'll get a prompt to accept the host key on
 # first connection unless we do this (since strict host key checking is on).
-p = subprocess.Popen(['/usr/bin/ssh-keyscan', '-H', ssh_hostname],
+p = subprocess.Popen(['/usr/bin/ssh-keyscan', '-H', 'hgssh'],
                      stdout=subprocess.PIPE)
 out = p.communicate()[0]
 if p.wait():
