97dcddc381ec7ba389b2bba6b04f0ec80cc27f34
Connor Sheehan <sheehan@mozilla.com>
2021-02-26 19:19:54 +0000

    terraform: upgrade to Terraform 0.13 (Bug 1695238) r=mhentges
    Ran `terraform 0.13upgrade`, and there are no changes except this
    new file.
    
    Differential Revision: https://phabricator.services.mozilla.com/D106624
    
    --HG--
    extra : moz-landing-system : lando

---
+++ terraform/versions.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+  required_version = ">= 0.13"
+}
