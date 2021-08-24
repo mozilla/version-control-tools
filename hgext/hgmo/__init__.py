be55eff0a1a9e8ab01fb330572415de3f3bcf8ec
Connor Sheehan <sheehan@mozilla.com>
2021-08-24 16:03:45 +0000

    hgext: mark some extensions as compatible with Mercurial 5.9 (Bug 1721228) r=zeid
    These extensions require no changes for 5.9 compatibility.
    
    Depends on D123472
    
    Differential Revision: https://phabricator.services.mozilla.com/D123473
    
    --HG--
    extra : moz-landing-system : lando

--- hgext/hgmo/__init__.py
+++ hgext/hgmo/__init__.py
@@ -76,7 +76,7 @@ from mozhg.util import (
 from mercurial.wireprotoserver import httpv1protocolhandler as webproto
 
 minimumhgversion = b'4.8'
-testedwith = b'4.8 4.9 5.0 5.1 5.2 5.3 5.4 5.5'
+testedwith = b'4.8 4.9 5.0 5.1 5.2 5.3 5.4 5.5 5.9'
 
 cmdtable = {}
 
