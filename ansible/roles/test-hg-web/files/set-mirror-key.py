34c96ba0a34725c799ab299e1c22be931e9f2a63
Connor Sheehan <sheehan@mozilla.com>
2021-11-06 22:11:21 -0400

    testing: write files in `w` mode instead of `wb`
    --HG--
    extra : rebase_source : 67a1bce72402e5716b4b1ccaf76528408e26b04a
    extra : histedit_source : 3013ab189c31c4b4bcf042d5600c20e03095999c

--- ansible/roles/test-hg-web/files/set-mirror-key.py
+++ ansible/roles/test-hg-web/files/set-mirror-key.py
@@ -11,10 +11,10 @@ import sys
 
 priv, pub, master_ip, master_key = sys.argv[1:]
 
-with open('/etc/mercurial/mirror', 'wb') as fh:
+with open('/etc/mercurial/mirror', 'w') as fh:
     fh.write(priv)
 
-with open('/etc/mercurial/mirror.pub', 'wb') as fh:
+with open('/etc/mercurial/mirror.pub', 'w') as fh:
     fh.write(pub)
 
 os.chmod('/etc/mercurial/mirror', stat.S_IRUSR | stat.S_IWUSR)
@@ -27,13 +27,13 @@ os.chown('/etc/mercurial/mirror', uhg.pw_uid, ghg.gr_gid)
 os.chown('/etc/mercurial/mirror.pub', uhg.pw_uid, ghg.gr_gid)
 
 # Allow SSH connections from the master server.
-with open('/home/hg/.ssh/authorized_keys', 'wb') as fh:
+with open('/home/hg/.ssh/authorized_keys', 'w') as fh:
     fh.write('command="/usr/local/bin/mirror-pull -t /repo_local/mozilla/mozilla $SSH_ORIGINAL_COMMAND"')
     fh.write(',no-pty,no-x11-forwarding,no-agent-forwarding ')
     fh.write(pub.strip())
     fh.write('\n')
 
-with open('/home/hg/.ssh/known_hosts', 'wb') as fh:
+with open('/home/hg/.ssh/known_hosts', 'w') as fh:
     fh.write('%s %s\n' % (master_ip, master_key))
 
 os.chown('/home/hg/.ssh/authorized_keys', uhg.pw_uid, ghg.gr_gid)
