ac02bac8912a6a4c7974f97d48f875b51c201a6d
Connor Sheehan <sheehan@mozilla.com>
2021-11-07 11:06:19 -0500

    testing: make set-mirrors.py Py3 compatible
    --HG--
    extra : rebase_source : 1c20a0836f786f10b5b312e2d44dd73cfd167d34
    extra : amend_source : 3e3cfae8dd21f4f3667adf7043e8059a12c7e3ef
    extra : histedit_source : 9a3e1990a743d6720520d4ba5936bd69c9e0b4ce

--- ansible/roles/test-hg-ssh/files/set-mirrors.py
+++ ansible/roles/test-hg-ssh/files/set-mirrors.py
@@ -3,14 +3,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import itertools
 import sys
 
-with open('/etc/mercurial/mirrors', 'wb') as mirrors:
-    with open('/etc/mercurial/known_hosts', 'wb') as kh:
+with open('/etc/mercurial/mirrors', 'w') as mirrors:
+    with open('/etc/mercurial/known_hosts', 'w') as kh:
         ips = sys.argv[1::2]
         keys = sys.argv[2::2]
-        for ip, key in itertools.izip(ips, keys):
+        for ip, key in zip(ips, keys):
             mirrors.write(ip)
             mirrors.write('\n')
 
